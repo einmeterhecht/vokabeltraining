130f8f5dda4ae553fed92ac259139211fc83bf20
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2023-03-14 00:52:09 +0100

    Final preparation for today's exam!

---
+++ abfragen/listen/franz/franz_2023-03-14_lastminute.js
@@ -0,0 +1,251 @@
+var liste = {
+    "fragen": [
+        {
+            "Franz\u00f6sisch": [
+                "falsch"
+            ],
+            "Deutsch": [
+                "faux, fausse"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Abgeordneter"
+            ],
+            "Deutsch": [
+                "d\u00e9put\u00e9"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Schl\u00e4gerei"
+            ],
+            "Deutsch": [
+                "une bagarre"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "etw bereuen"
+            ],
+            "Deutsch": [
+                "se repentir de qc"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "fordern"
+            ],
+            "Deutsch": [
+                "revendiquer"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ermitteln"
+            ],
+            "Deutsch": [
+                "enqu\u00eater"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "klauen"
+            ],
+            "Deutsch": [
+                "piquer"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Polizist"
+            ],
+            "Deutsch": [
+                "gendarme"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "L\u00f6segeld"
+            ],
+            "Deutsch": [
+                "une ran\u00e7on"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Motif"
+            ],
+            "Deutsch": [
+                "un mobile"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Strafverfolgung"
+            ],
+            "Deutsch": [
+                "les poursuites"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Verh\u00f6r"
+            ],
+            "Deutsch": [
+                "un interrogatoire"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Enthaltung"
+            ],
+            "Deutsch": [
+                "abstention"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Streikender"
+            ],
+            "Deutsch": [
+                "gr\u00e9viste"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Einbrecher"
+            ],
+            "Deutsch": [
+                "cambrioleur"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Razzia"
+            ],
+            "Deutsch": [
+                "une rafle"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "in Betracht ziehen"
+            ],
+            "Deutsch": [
+                "envisager"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Vereinbarung"
+            ],
+            "Deutsch": [
+                "un accord"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Verdacht"
+            ],
+            "Deutsch": [
+                "un soup\u00e7on"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "die Arbeit niederlegen"
+            ],
+            "Deutsch": [
+                "d\u00e9brayer"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "verfolgen"
+            ],
+            "Deutsch": [
+                "pers\u00e9cuter; poursuivre"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Schweizerische Eidgenossenschaft"
+            ],
+            "Deutsch": [
+                "la Conf\u00e9d\u00e9ration helv\u00e9tique"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "f\u00fchren"
+            ],
+            "Deutsch": [
+                "diriger"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Steuerbeamter/Finanzamt"
+            ],
+            "Deutsch": [
+                "percepteur"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Rathaus"
+            ],
+            "Deutsch": [
+                "un h\u00f4tel de ville"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Wahlkreis"
+            ],
+            "Deutsch": [
+                "une circonscription"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Freispruch"
+            ],
+            "Deutsch": [
+                "un acquittement"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "auf Bew\u00e4hrung"
+            ],
+            "Deutsch": [
+                "avec sursis"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Busse"
+            ],
+            "Deutsch": [
+                "une amende"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Tatverd\u00e4chtiger, Beschuldigter"
+            ],
+            "Deutsch": [
+                "pr\u00e9venu"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele"
+    ],
+    "titel": "franz_2023-03-14_lastminute",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
