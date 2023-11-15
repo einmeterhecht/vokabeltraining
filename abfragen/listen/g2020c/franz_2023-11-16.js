05349c494f2f71e9daa928e20dda71368559ed22
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2023-11-15 17:21:01 +0100

    Add franz_2023-11-16

---
+++ abfragen/listen/g2020c/franz_2023-11-16.js
@@ -0,0 +1,708 @@
+var liste = {
+    "fragen": [
+        {
+            "Deutsch": [
+                "eine Beziehung"
+            ],
+            "Franz\u00f6sisch": [
+                "une relation"
+            ]
+        },
+        {
+            "Deutsch": [
+                "deutsch-franz\u00f6sisch"
+            ],
+            "Franz\u00f6sisch": [
+                "franco-allemand/e"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Parlament"
+            ],
+            "Franz\u00f6sisch": [
+                "un parlement"
+            ]
+        },
+        {
+            "Deutsch": [
+                "europ\u00e4isch"
+            ],
+            "Franz\u00f6sisch": [
+                "europ\u00e9en/europ\u00e9enne"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Pr\u00e4sident"
+            ],
+            "Franz\u00f6sisch": [
+                "un pr\u00e9sident"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Pr\u00e4sidentin"
+            ],
+            "Franz\u00f6sisch": [
+                "une pr\u00e9sidente"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Republik"
+            ],
+            "Franz\u00f6sisch": [
+                "une r\u00e9publique"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Kanzler"
+            ],
+            "Franz\u00f6sisch": [
+                "un chancelier"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Kanzlerin"
+            ],
+            "Franz\u00f6sisch": [
+                "une chanceli\u00e8re"
+            ]
+        },
+        {
+            "Deutsch": [
+                "Welt-"
+            ],
+            "Franz\u00f6sisch": [
+                "mondial/mondiale/mondiaux/mondiales"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Abhandlung; ein Vertrag"
+            ],
+            "Franz\u00f6sisch": [
+                "un trait\u00e9"
+            ]
+        },
+        {
+            "Deutsch": [
+                "zweiter/zweite/zweites"
+            ],
+            "Franz\u00f6sisch": [
+                "second/seconde"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Zahl"
+            ],
+            "Franz\u00f6sisch": [
+                "un nombre"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Toter/eine Tote"
+            ],
+            "Franz\u00f6sisch": [
+                "un mort/une morte"
+            ]
+        },
+        {
+            "Deutsch": [
+                "sich kennen"
+            ],
+            "Franz\u00f6sisch": [
+                "se conna\u00eetre"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Teilung"
+            ],
+            "Franz\u00f6sisch": [
+                "une division"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Geburt"
+            ],
+            "Franz\u00f6sisch": [
+                "une naissance"
+            ]
+        },
+        {
+            "Deutsch": [
+                "das DFJW"
+            ],
+            "Franz\u00f6sisch": [
+                "l'OFAJ (Office franco-allemand pour la jeunesse)"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Fall/Sturz"
+            ],
+            "Franz\u00f6sisch": [
+                "une chute"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Mauer; eine Wand"
+            ],
+            "Franz\u00f6sisch": [
+                "un mur"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Els\u00e4sser/eine Els\u00e4sserin"
+            ],
+            "Franz\u00f6sisch": [
+                "un Alsacien/une Alsacienne"
+            ]
+        },
+        {
+            "Deutsch": [
+                "trotz"
+            ],
+            "Franz\u00f6sisch": [
+                "malgr\u00e9"
+            ]
+        },
+        {
+            "Deutsch": [
+                "das Elsass"
+            ],
+            "Franz\u00f6sisch": [
+                "l'Alsace (f)"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. verhindern"
+            ],
+            "Franz\u00f6sisch": [
+                "emp\u00eacher qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "jdn. daran hindern etw. zu tun"
+            ],
+            "Franz\u00f6sisch": [
+                "emp\u00eacher qn de faire qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. verhindern"
+            ],
+            "Franz\u00f6sisch": [
+                "emp\u00eacher qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Gedicht"
+            ],
+            "Franz\u00f6sisch": [
+                "un po\u00e8me"
+            ]
+        },
+        {
+            "Deutsch": [
+                "sich an etw. anpassem"
+            ],
+            "Franz\u00f6sisch": [
+                "s'adapter \u00e0 qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "der Herbst"
+            ],
+            "Franz\u00f6sisch": [
+                "l'automne (m)"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. ersetzen"
+            ],
+            "Franz\u00f6sisch": [
+                "remplacer qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "das Ausland"
+            ],
+            "Franz\u00f6sisch": [
+                "l'\u00e9tranger m."
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Erfahrung"
+            ],
+            "Franz\u00f6sisch": [
+                "une exp\u00e9rience"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Empfang"
+            ],
+            "Franz\u00f6sisch": [
+                "un accueil"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Gastfamilie"
+            ],
+            "Franz\u00f6sisch": [
+                "une famille d'accueil"
+            ]
+        },
+        {
+            "Deutsch": [
+                "anders sein als jd."
+            ],
+            "Franz\u00f6sisch": [
+                "\u00eatre diff\u00e9rent/diff\u00e9rente de qn"
+            ]
+        },
+        {
+            "Deutsch": [
+                "weit weg von etw."
+            ],
+            "Franz\u00f6sisch": [
+                "loin de qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "um etwas nicht zu tun"
+            ],
+            "Franz\u00f6sisch": [
+                "pour ne pas faire qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "Schwierigkeiten haben, etw. zu tun"
+            ],
+            "Franz\u00f6sisch": [
+                "avoir des difficult\u00e9s \u00e0 faire qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "sich (weiter) entwicklen"
+            ],
+            "Franz\u00f6sisch": [
+                "\u00e9voluer"
+            ]
+        },
+        {
+            "Deutsch": [
+                "bevor man etw. tut"
+            ],
+            "Franz\u00f6sisch": [
+                "avant de faire qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "tolerant"
+            ],
+            "Franz\u00f6sisch": [
+                "tol\u00e9rant/tol\u00e9rante"
+            ]
+        },
+        {
+            "Deutsch": [
+                "nachdem man etw. getan hat"
+            ],
+            "Franz\u00f6sisch": [
+                "apr\u00e8s avoir fait qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "offen"
+            ],
+            "Franz\u00f6sisch": [
+                "ouvert/ouverte"
+            ]
+        },
+        {
+            "Deutsch": [
+                "selbst\u00e4ndig/unabh\u00e4ngig/autonom"
+            ],
+            "Franz\u00f6sisch": [
+                "autonome"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein (Schul)Fach"
+            ],
+            "Franz\u00f6sisch": [
+                "une mati\u00e8re"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eines Tages"
+            ],
+            "Franz\u00f6sisch": [
+                "un jour"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. verbessern"
+            ],
+            "Franz\u00f6sisch": [
+                "am\u00e9liorer qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "sich f\u00fcr etw. interessieren"
+            ],
+            "Franz\u00f6sisch": [
+                "s'int\u00e9resser \u00e0 qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Referat"
+            ],
+            "Franz\u00f6sisch": [
+                "un expos\u00e9"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Bed\u00fcrfnis"
+            ],
+            "Franz\u00f6sisch": [
+                "un besoin"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. brauchen; etw. tun m\u00fcssen"
+            ],
+            "Franz\u00f6sisch": [
+                "avoir besoin de qc/de faire qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "am Vortag/tags zuvor"
+            ],
+            "Franz\u00f6sisch": [
+                "la veille"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. hinzuf\u00fcgen"
+            ],
+            "Franz\u00f6sisch": [
+                "ajouter qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "Widerstandsbewegung im 2. Weltkrieg"
+            ],
+            "Franz\u00f6sisch": [
+                "la R\u00e9sistance"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. schaffen, gr\u00fcnden"
+            ],
+            "Franz\u00f6sisch": [
+                "cr\u00e9er qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "Kontakt aufnehmen zu jdm."
+            ],
+            "Franz\u00f6sisch": [
+                "prendre contact avec qn"
+            ]
+        },
+        {
+            "Deutsch": [
+                "sich treffen"
+            ],
+            "Franz\u00f6sisch": [
+                "se rencontrer"
+            ]
+        },
+        {
+            "Deutsch": [
+                "handeln"
+            ],
+            "Franz\u00f6sisch": [
+                "agir"
+            ]
+        },
+        {
+            "Deutsch": [
+                "politisch"
+            ],
+            "Franz\u00f6sisch": [
+                "politique"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Politiker/eine Politikerin"
+            ],
+            "Franz\u00f6sisch": [
+                "un homme politique/une femme politique"
+            ]
+        },
+        {
+            "Deutsch": [
+                "geboren werden"
+            ],
+            "Franz\u00f6sisch": [
+                "na\u00eetre"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. kontrollieren"
+            ],
+            "Franz\u00f6sisch": [
+                "contr\u00f4ler qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "denn"
+            ],
+            "Franz\u00f6sisch": [
+                "car"
+            ]
+        },
+        {
+            "Deutsch": [
+                "die Industrie"
+            ],
+            "Franz\u00f6sisch": [
+                "l'industrie (f)"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Waffe"
+            ],
+            "Franz\u00f6sisch": [
+                "une arme"
+            ]
+        },
+        {
+            "Deutsch": [
+                "jdn./etw. betreffen"
+            ],
+            "Franz\u00f6sisch": [
+                "concerner qn/qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. unterschreiben"
+            ],
+            "Franz\u00f6sisch": [
+                "signer qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Pass"
+            ],
+            "Franz\u00f6sisch": [
+                "un passeport"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Ware"
+            ],
+            "Franz\u00f6sisch": [
+                "une marchandise"
+            ]
+        },
+        {
+            "Deutsch": [
+                "die Europ\u00e4ische Union"
+            ],
+            "Franz\u00f6sisch": [
+                "l'Union europ\u00e9enne f."
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine W\u00e4hrung"
+            ],
+            "Franz\u00f6sisch": [
+                "une monnaie"
+            ]
+        },
+        {
+            "Deutsch": [
+                "auch wenn"
+            ],
+            "Franz\u00f6sisch": [
+                "m\u00eame si"
+            ]
+        },
+        {
+            "Deutsch": [
+                "vorw\u00e4rts kommen"
+            ],
+            "Franz\u00f6sisch": [
+                "avancer"
+            ]
+        },
+        {
+            "Deutsch": [
+                "jdn. aufnehmen; jdn. empfangen"
+            ],
+            "Franz\u00f6sisch": [
+                "accueillir qn"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Ankunft"
+            ],
+            "Franz\u00f6sisch": [
+                "une arriv\u00e9e"
+            ]
+        },
+        {
+            "Deutsch": [
+                "es schaffen etw. zu tun"
+            ],
+            "Franz\u00f6sisch": [
+                "r\u00e9ussir \u00e0 faire qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "der/die/das wenigste; am wenigsten"
+            ],
+            "Franz\u00f6sisch": [
+                "le moins de"
+            ]
+        },
+        {
+            "Deutsch": [
+                "etw. bedeuten/heissen"
+            ],
+            "Franz\u00f6sisch": [
+                "signifier qc"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Klischee"
+            ],
+            "Franz\u00f6sisch": [
+                "un clich\u00e9"
+            ]
+        },
+        {
+            "Deutsch": [
+                "typisch"
+            ],
+            "Franz\u00f6sisch": [
+                "typiquement"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Bier"
+            ],
+            "Franz\u00f6sisch": [
+                "une bi\u00e8re"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Wurst"
+            ],
+            "Franz\u00f6sisch": [
+                "une saucisse"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Thema"
+            ],
+            "Franz\u00f6sisch": [
+                "un sujet"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Tafel/Tafeln"
+            ],
+            "Franz\u00f6sisch": [
+                "un tableau/des tableaux"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Gliederung"
+            ],
+            "Franz\u00f6sisch": [
+                "un plan"
+            ]
+        },
+        {
+            "Deutsch": [
+                "ein Blatt"
+            ],
+            "Franz\u00f6sisch": [
+                "une feuille"
+            ]
+        },
+        {
+            "Deutsch": [
+                "eine Folie"
+            ],
+            "Franz\u00f6sisch": [
+                "un transparent"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "franz_2023-11-16",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
