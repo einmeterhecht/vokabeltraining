37f3d68cfdb2657e60c1ade928593c57b59712bc
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2024-01-15 01:21:45 +0100

    Create franz_2024-01-16.js

---
+++ abfragen/listen/g2020c/franz_2024-01-16.js
@@ -0,0 +1,684 @@
+var liste = {
+    "fragen": [
+        {
+            "Franz\u00f6sisch": [
+                "un plan"
+            ],
+            "Deutsch": [
+                "eine (Kamera)Einstellung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un gros plan"
+            ],
+            "Deutsch": [
+                "eine Grossaufnahme"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "inspirer qn"
+            ],
+            "Deutsch": [
+                "jdn. inspirieren"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un r\u00e9alisateur/une r\u00e9alisatrice"
+            ],
+            "Deutsch": [
+                "ein (Film)Regisseur/eine (Film)Regisseurin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "entier, enti\u00e8re"
+            ],
+            "Deutsch": [
+                "ganz, v\u00f6llig"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un court m\u00e9trage"
+            ],
+            "Deutsch": [
+                "ein Kurzfilm"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "tourner (un film)"
+            ],
+            "Deutsch": [
+                "einen Film drehen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un arrondissement"
+            ],
+            "Deutsch": [
+                "ein Stadtbezirk"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "c'est/ ce sont... qui"
+            ],
+            "Deutsch": [
+                "Wendung zur Hervorhebung eines Subjekts"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "avoir qc pour th\u00e8me"
+            ],
+            "Deutsch": [
+                "etw. zum Thema haben"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une rencontre"
+            ],
+            "Deutsch": [
+                "eine Begegnung/ein Treffen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un quai"
+            ],
+            "Deutsch": [
+                "eine Uferstrasse/ein Kai"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "se d\u00e9rouler"
+            ],
+            "Deutsch": [
+                "sich abspielen/spielen/stattfinden"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une s\u00e9quence"
+            ],
+            "Deutsch": [
+                "eine (Film)Sequenz/Abfolge"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "bless\u00e9, bless\u00e9e"
+            ],
+            "Deutsch": [
+                "verletzt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une image"
+            ],
+            "Deutsch": [
+                "ein Bild"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un sc\u00e9nario"
+            ],
+            "Deutsch": [
+                "ein Drehbuch; eine Story"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un dialogue"
+            ],
+            "Deutsch": [
+                "ein Dialog"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Voici..."
+            ],
+            "Deutsch": [
+                "Hier ist/ Hier sind"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une description"
+            ],
+            "Deutsch": [
+                "eine Beschreibung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "recevoir qc, qn"
+            ],
+            "Deutsch": [
+                "etw. (ab)bekommen/erhalten; jdn. empfangen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un coup de couteau"
+            ],
+            "Deutsch": [
+                "ein Messerstich"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "soigner qn"
+            ],
+            "Deutsch": [
+                "jdn. pflegen; jdn. versorgen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la tendresse"
+            ],
+            "Deutsch": [
+                "die Z\u00e4rtlichkeit"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "prudent, prudente"
+            ],
+            "Deutsch": [
+                "vorsichtig"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un lieu/des lieux"
+            ],
+            "Deutsch": [
+                "ein Ort/Orte"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00e0 l'int\u00e9rieur"
+            ],
+            "Deutsch": [
+                "(nach) drinnen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00e0 l'ext\u00e9rieur"
+            ],
+            "Deutsch": [
+                "(nach) draussen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "de jour"
+            ],
+            "Deutsch": [
+                "am Tag/tags\u00fcber"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un parking (souterrain)"
+            ],
+            "Deutsch": [
+                "ein Parkplatz; eine Tiefgarage"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un dictionnaire"
+            ],
+            "Deutsch": [
+                "ein W\u00f6rterbuch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "d\u00e9couper qc"
+            ],
+            "Deutsch": [
+                "etw. zerlegen/in St\u00fccke schneiden; etw. ausschneiden"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un dictionnaire"
+            ],
+            "Deutsch": [
+                "ein W\u00f6rterbuch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "d\u00e9couper qc"
+            ],
+            "Deutsch": [
+                "etw . zerlegen / in St\u00fccke schneiden etw ausschneiden"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "c'est - \u00e0 - dire"
+            ],
+            "Deutsch": [
+                "das hei\u00dft"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "couper qc"
+            ],
+            "Deutsch": [
+                "etw . schneiden"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "en entier"
+            ],
+            "Deutsch": [
+                "ganz/ vollst\u00e4ndig"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un plan d'ensemble"
+            ],
+            "Deutsch": [
+                "eine totale Einstellung (Filmsprache)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "c'est/ ce sont que"
+            ],
+            "Deutsch": [
+                "Wendung zur Hervorhebung eines Objekts oder eines Adverbs"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "g\u00e9n\u00e9ral/g\u00e9n\u00e9rale/g\u00e9n\u00e9raux/g\u00e9n\u00e9rales"
+            ],
+            "Deutsch": [
+                "allgemein"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un plan rapproch\u00e9"
+            ],
+            "Deutsch": [
+                "eine Nahaufnahme"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "dont"
+            ],
+            "Deutsch": [
+                "dessen/ deren: von dem/ von der/ von denen (Relativpronomen)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "pr\u00e9cis, pr\u00e9cise"
+            ],
+            "Deutsch": [
+                "genau"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une taille"
+            ],
+            "Deutsch": [
+                "eine Taille"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une r\u00e9action"
+            ],
+            "Deutsch": [
+                "eine Reaktion"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "de pr\u00e8s"
+            ],
+            "Deutsch": [
+                "aus der N\u00e4he"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un public"
+            ],
+            "Deutsch": [
+                "ein Publikum"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un mouvement"
+            ],
+            "Deutsch": [
+                "eine Bewegung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un cameraman"
+            ],
+            "Deutsch": [
+                "ein Kameramann/eine Kamerafrau"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un sol"
+            ],
+            "Deutsch": [
+                "ein Boden"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la plong\u00e9e"
+            ],
+            "Deutsch": [
+                "die Aufsicht/Vogelperspektive (Filmsprache: Aufnahme von oben)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une impression"
+            ],
+            "Deutsch": [
+                "ein Eindruck"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la contre-plong\u00e9e"
+            ],
+            "Deutsch": [
+                "die Untersicht/Froschperspektive (Filmsprache: Aufnahme von unten)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "dominer qc"
+            ],
+            "Deutsch": [
+                "etw. beherrschen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une perspective"
+            ],
+            "Deutsch": [
+                "eine Perspektive"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "subjectif/subjective"
+            ],
+            "Deutsch": [
+                "subjektiv"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "objectif, objective"
+            ],
+            "Deutsch": [
+                "objektiv"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "neutre"
+            ],
+            "Deutsch": [
+                "neutral"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un tournage"
+            ],
+            "Deutsch": [
+                "Dreharbeiten"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une critique"
+            ],
+            "Deutsch": [
+                "eine Kritik"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un film policier"
+            ],
+            "Deutsch": [
+                "ein Kriminalfilm"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un film dramatique"
+            ],
+            "Deutsch": [
+                "ein Filmdrama"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un film fantastique"
+            ],
+            "Deutsch": [
+                "ein Fantasiefilm"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une com\u00e9die"
+            ],
+            "Deutsch": [
+                "eine Kom\u00f6die"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un film d'action"
+            ],
+            "Deutsch": [
+                "ein Actionfilm"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un film d'horreur"
+            ],
+            "Deutsch": [
+                "ein Horrorfilm"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un film d'animation"
+            ],
+            "Deutsch": [
+                "ein Animationsfilm"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "d'apr\u00e8s qn"
+            ],
+            "Deutsch": [
+                "nach jmd./ gem\u00e4ss"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un long m\u00e9trage"
+            ],
+            "Deutsch": [
+                "ein Spielfilm"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00e9mouvant, \u00e9mouvante"
+            ],
+            "Deutsch": [
+                "bewegend, ergreifend"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "amusant, amusante"
+            ],
+            "Deutsch": [
+                "unterhaltsam, lustig"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ennuyeux/ennuyeuse"
+            ],
+            "Deutsch": [
+                "langweilig"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "s'identifier \u00e0 qn"
+            ],
+            "Deutsch": [
+                "sich mit jdm. identifizieren"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "accompagner qn"
+            ],
+            "Deutsch": [
+                "jdn. begleiten"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "rater qc"
+            ],
+            "Deutsch": [
+                "etw. verpassen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "d\u00e9cevoir qn"
+            ],
+            "Deutsch": [
+                "jdn entt\u00e4uschen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "draguer qn"
+            ],
+            "Deutsch": [
+                "jdn anmachen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "s'en aller"
+            ],
+            "Deutsch": [
+                "weggehen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "lancer qc \u00e0 qn"
+            ],
+            "Deutsch": [
+                "jdm. etw. zuwerfen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un sourire"
+            ],
+            "Deutsch": [
+                "ein L\u00e4cheln"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00eatre oblig\u00e9/oblig\u00e9e de faire qc"
+            ],
+            "Deutsch": [
+                "etw. tun m\u00fcssen/verpflichtet sein, etw. zu tun"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un truc"
+            ],
+            "Deutsch": [
+                "ein Ding"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "arriver \u00e0 faire qc"
+            ],
+            "Deutsch": [
+                "es schaffen, etw. zu tun"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une mosqu\u00e9e"
+            ],
+            "Deutsch": [
+                "eine Moschee"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "franz_2024-01-16",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
