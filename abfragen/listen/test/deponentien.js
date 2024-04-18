2b417f15adf7eb6fbd0b457df8951733a20f1d1c
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2024-04-18 17:38:06 +0200

    use different file for testing

---
+++ abfragen/listen/test/deponentien.js
@@ -0,0 +1,44 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "aggredi (aggredior aggressus sum)"
+            ],
+            "Deutsch": [
+                "angreifen; herangehen"
+            ]
+        },
+        {
+            "Latein": [
+                "arbitrari (arbitror arbitratus sum)"
+            ],
+            "Deutsch": [
+                "glauben; meinen"
+            ]
+        },
+        {
+            "Latein": [
+                "audere (audeo ausus sum)"
+            ],
+            "Deutsch": [
+                "wagen"
+            ],
+            "Lernhilfe": [
+                "(Semideponens)"
+            ]
+        },
+        {
+            "Latein": [
+                "conari (conor conatus sum)"
+            ],
+            "Deutsch": [
+                "versuchen"
+            ]
+        }
+    ],
+    "hinweis_attribute": ["Lernhilfe"],
+    "titel": "Deponentien",
+    "frage_attribut": "Latein"
+};
+
+erste_aufgabe();
