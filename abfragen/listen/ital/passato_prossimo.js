c22484469816338de22b26e7f6fb142e9510ad29
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2024-04-22 00:40:55 +0200

    Add ital/passato_prossimo

---
+++ abfragen/listen/ital/passato_prossimo.js
@@ -0,0 +1,428 @@
+var liste = {
+    "fragen": [
+        {
+            "Infinito": [
+                "bere"
+            ],
+            "Passato_prossimo": [
+                "(ho) bevuto"
+            ]
+        },
+        {
+            "Infinito": [
+                "vivere"
+            ],
+            "Passato_prossimo": [
+                "(ho) vissuto"
+            ]
+        },
+        {
+            "Infinito": [
+                "venire"
+            ],
+            "Passato_prossimo": [
+                "(sono) venuto"
+            ]
+        },
+        {
+            "Infinito": [
+                "vedere"
+            ],
+            "Passato_prossimo": [
+                "(ho) visto"
+            ]
+        },
+        {
+            "Infinito": [
+                "chiedere"
+            ],
+            "Passato_prossimo": [
+                "(ho) chiesto"
+            ]
+        },
+        {
+            "Infinito": [
+                "rispondere"
+            ],
+            "Passato_prossimo": [
+                "(ho) risposto"
+            ]
+        },
+        {
+            "Infinito": [
+                "(pro)porre"
+            ],
+            "Passato_prossimo": [
+                "(ho) (pro)posto"
+            ]
+        },
+        {
+            "Infinito": [
+                "nascondere"
+            ],
+            "Passato_prossimo": [
+                "(ho) nascosto"
+            ]
+        },
+        {
+            "Infinito": [
+                "rimanere"
+            ],
+            "Passato_prossimo": [
+                "(sono) rimasto"
+            ]
+        },
+        {
+            "Infinito": [
+                "dire"
+            ],
+            "Passato_prossimo": [
+                "(ho) detto"
+            ]
+        },
+        {
+            "Infinito": [
+                "fare"
+            ],
+            "Passato_prossimo": [
+                "(ho) fatto"
+            ]
+        },
+        {
+            "Infinito": [
+                "leggere"
+            ],
+            "Passato_prossimo": [
+                "(ho) letto"
+            ]
+        },
+        {
+            "Infinito": [
+                "rompere"
+            ],
+            "Passato_prossimo": [
+                "(ho) rotto"
+            ]
+        },
+        {
+            "Infinito": [
+                "scrivere"
+            ],
+            "Passato_prossimo": [
+                "(ho) scritto"
+            ]
+        },
+        {
+            "Infinito": [
+                "tradurre"
+            ],
+            "Passato_prossimo": [
+                "(ho) tradotto"
+            ]
+        },
+        {
+            "Infinito": [
+                "condurre"
+            ],
+            "Passato_prossimo": [
+                "(ho) condotto"
+            ]
+        },
+        {
+            "Infinito": [
+                "trarre"
+            ],
+            "Passato_prossimo": [
+                "(ho) tratto"
+            ]
+        },
+        {
+            "Infinito": [
+                "correggere"
+            ],
+            "Passato_prossimo": [
+                "(ho) corretto"
+            ]
+        },
+        {
+            "Infinito": [
+                "proteggere"
+            ],
+            "Passato_prossimo": [
+                "(ho) protetto"
+            ]
+        },
+        {
+            "Infinito": [
+                "produrre"
+            ],
+            "Passato_prossimo": [
+                "(ho) prodotto"
+            ]
+        },
+        {
+            "Infinito": [
+                "essere"
+            ],
+            "Passato_prossimo": [
+                "(sono) stato"
+            ]
+        },
+        {
+            "Infinito": [
+                "stare"
+            ],
+            "Passato_prossimo": [
+                "(sono) stato"
+            ]
+        },
+        {
+            "Infinito": [
+                "nascere"
+            ],
+            "Passato_prossimo": [
+                "(sono) nato"
+            ]
+        },
+        {
+            "Infinito": [
+                "discutere"
+            ],
+            "Passato_prossimo": [
+                "(ho) discusso"
+            ]
+        },
+        {
+            "Infinito": [
+                "mettere"
+            ],
+            "Passato_prossimo": [
+                "(ho) messo"
+            ]
+        },
+        {
+            "Infinito": [
+                "esprimere"
+            ],
+            "Passato_prossimo": [
+                "(ho) espresso"
+            ]
+        },
+        {
+            "Infinito": [
+                "succedere"
+            ],
+            "Passato_prossimo": [
+                "\u00e8 successo"
+            ]
+        },
+        {
+            "Infinito": [
+                "muovere"
+            ],
+            "Passato_prossimo": [
+                "(ho) mosso"
+            ]
+        },
+        {
+            "Infinito": [
+                "accendere"
+            ],
+            "Passato_prossimo": [
+                "(ho) acceso"
+            ]
+        },
+        {
+            "Infinito": [
+                "spendere"
+            ],
+            "Passato_prossimo": [
+                "(ho) speso"
+            ]
+        },
+        {
+            "Infinito": [
+                "prendere"
+            ],
+            "Passato_prossimo": [
+                "(ho) preso"
+            ]
+        },
+        {
+            "Infinito": [
+                "uccidere"
+            ],
+            "Passato_prossimo": [
+                "(ho) ucciso"
+            ]
+        },
+        {
+            "Infinito": [
+                "correre"
+            ],
+            "Passato_prossimo": [
+                "(ho/sono) corso"
+            ]
+        },
+        {
+            "Infinito": [
+                "chiudere"
+            ],
+            "Passato_prossimo": [
+                "(ho) chiuso"
+            ]
+        },
+        {
+            "Infinito": [
+                "decidere"
+            ],
+            "Passato_prossimo": [
+                "(ho) deciso"
+            ]
+        },
+        {
+            "Infinito": [
+                "perdere"
+            ],
+            "Passato_prossimo": [
+                "(ho) perso"
+            ]
+        },
+        {
+            "Infinito": [
+                "dividere"
+            ],
+            "Passato_prossimo": [
+                "(ho) diviso"
+            ]
+        },
+        {
+            "Infinito": [
+                "scendere"
+            ],
+            "Passato_prossimo": [
+                "(sono) sceso"
+            ]
+        },
+        {
+            "Infinito": [
+                "ridere"
+            ],
+            "Passato_prossimo": [
+                "(ho) riso"
+            ]
+        },
+        {
+            "Infinito": [
+                "fondere"
+            ],
+            "Passato_prossimo": [
+                "(ho) fuso"
+            ]
+        },
+        {
+            "Infinito": [
+                "(con)vincere"
+            ],
+            "Passato_prossimo": [
+                "(ho) (con)vinto"
+            ]
+        },
+        {
+            "Infinito": [
+                "piangere"
+            ],
+            "Passato_prossimo": [
+                "(ho) pianto"
+            ]
+        },
+        {
+            "Infinito": [
+                "spegnere"
+            ],
+            "Passato_prossimo": [
+                "(ho) spento"
+            ]
+        },
+        {
+            "Infinito": [
+                "dipingere"
+            ],
+            "Passato_prossimo": [
+                "(ho) dipinto"
+            ]
+        },
+        {
+            "Infinito": [
+                "scegliere"
+            ],
+            "Passato_prossimo": [
+                "(ho) scelto"
+            ]
+        },
+        {
+            "Infinito": [
+                "(r)accogliere"
+            ],
+            "Passato_prossimo": [
+                "(ho) (r)accolto"
+            ]
+        },
+        {
+            "Infinito": [
+                "togliere"
+            ],
+            "Passato_prossimo": [
+                "(ho) tolto"
+            ]
+        },
+        {
+            "Infinito": [
+                "aprire"
+            ],
+            "Passato_prossimo": [
+                "(ho) aperto"
+            ]
+        },
+        {
+            "Infinito": [
+                "(s)coprire"
+            ],
+            "Passato_prossimo": [
+                "(ho) (s)coperto"
+            ]
+        },
+        {
+            "Infinito": [
+                "offrire"
+            ],
+            "Passato_prossimo": [
+                "(ho) offerto"
+            ]
+        },
+        {
+            "Infinito": [
+                "soffrire"
+            ],
+            "Passato_prossimo": [
+                "(ho) sofferto"
+            ]
+        },
+        {
+            "Infinito": [
+                "morire"
+            ],
+            "Passato_prossimo": [
+                "(sono) morto"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "passato_prossimo",
+    "frage_attribut": "Infinito"
+};
+
+erste_aufgabe();
