84c36883cc580fabb4a6cdc7e727a74ad241b670
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2024-06-07 19:44:27 +0200

    Add latein_viva_10

---
+++ abfragen/listen/g2023k/latein_viva_10.js
@@ -0,0 +1,212 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "alius"
+            ],
+            "Deutsch": [
+                "ein anderer"
+            ]
+        },
+        {
+            "Latein": [
+                "c\u0101rus"
+            ],
+            "Deutsch": [
+                "teuer, wertvoll, lieb"
+            ]
+        },
+        {
+            "Latein": [
+                "cupidit\u0101s"
+            ],
+            "Deutsch": [
+                "Begierde, Leidenschaft"
+            ]
+        },
+        {
+            "Latein": [
+                "cupidus"
+            ],
+            "Deutsch": [
+                "gierig (auf etw.)"
+            ]
+        },
+        {
+            "Latein": [
+                "d\u012bgnus"
+            ],
+            "Deutsch": [
+                "w\u00fcrdig "
+            ]
+        },
+        {
+            "Latein": [
+                "eques"
+            ],
+            "Deutsch": [
+                "Reiter, Ritter"
+            ]
+        },
+        {
+            "Latein": [
+                "erg\u014d"
+            ],
+            "Deutsch": [
+                "also"
+            ]
+        },
+        {
+            "Latein": [
+                "forum"
+            ],
+            "Deutsch": [
+                "Forum, Marktplatz"
+            ]
+        },
+        {
+            "Latein": [
+                "id\u014dneus"
+            ],
+            "Deutsch": [
+                "geeignet (f\u00fcr etw.)"
+            ]
+        },
+        {
+            "Latein": [
+                "intellegere"
+            ],
+            "Deutsch": [
+                "bemerken, verstehen"
+            ]
+        },
+        {
+            "Latein": [
+                "is"
+            ],
+            "Deutsch": [
+                "der, dieser, er"
+            ]
+        },
+        {
+            "Latein": [
+                "minim\u0113"
+            ],
+            "Deutsch": [
+                "ganz und gar nicht, am wenigsten"
+            ]
+        },
+        {
+            "Latein": [
+                "mon\u0113re"
+            ],
+            "Deutsch": [
+                "(er)mahnen"
+            ]
+        },
+        {
+            "Latein": [
+                "neglegere"
+            ],
+            "Deutsch": [
+                "nicht beachten, missachten, vernachl\u00e4ssigen"
+            ]
+        },
+        {
+            "Latein": [
+                "novus"
+            ],
+            "Deutsch": [
+                "neu"
+            ]
+        },
+        {
+            "Latein": [
+                "numquam"
+            ],
+            "Deutsch": [
+                "niemals"
+            ]
+        },
+        {
+            "Latein": [
+                "\u014drn\u0101mentum"
+            ],
+            "Deutsch": [
+                "Schmuck"
+            ]
+        },
+        {
+            "Latein": [
+                "p\u0101nis"
+            ],
+            "Deutsch": [
+                "Brot"
+            ]
+        },
+        {
+            "Latein": [
+                "pec\u016bnia"
+            ],
+            "Deutsch": [
+                "Geld"
+            ]
+        },
+        {
+            "Latein": [
+                "pretium"
+            ],
+            "Deutsch": [
+                "Preis, Lohn"
+            ]
+        },
+        {
+            "Latein": [
+                "profect\u014d"
+            ],
+            "Deutsch": [
+                "in der Tat, sicherlich"
+            ]
+        },
+        {
+            "Latein": [
+                "quid"
+            ],
+            "Deutsch": [
+                "was?"
+            ]
+        },
+        {
+            "Latein": [
+                "toga"
+            ],
+            "Deutsch": [
+                "Toga"
+            ]
+        },
+        {
+            "Latein": [
+                "varius"
+            ],
+            "Deutsch": [
+                "verschieden, bunt, vielf\u00e4ltig"
+            ]
+        },
+        {
+            "Latein": [
+                "vestis"
+            ],
+            "Deutsch": [
+                "Bekleidung"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "latein_viva_10",
+    "frage_attribut": "Latein"
+};
+
+erste_aufgabe();
