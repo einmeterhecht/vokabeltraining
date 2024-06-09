6178bc466fbbd3a3c583138e73ad7bb1dcb8c958
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2024-06-09 10:47:58 +0200

    Add grammar info to latein_viva-5-6

--- abfragen/listen/g2023k/latein_viva_5-6.js
+++ abfragen/listen/g2023k/latein_viva_5-6.js
@@ -1,427 +1,427 @@
 var liste = {
     "fragen": [
         {
-            "Latein": [
-                "\u0101"
-            ],
             "Deutsch": [
                 "von, von etw. her"
+            ],
+            "Latein": [
+                "\u0101 (mit Abl.)"
             ]
         },
         {
-            "Latein": [
-                "ab"
-            ],
             "Deutsch": [
                 "von, von etw. her"
+            ],
+            "Latein": [
+                "ab (mit Abl.)"
             ]
         },
         {
-            "Latein": [
-                "advers\u0101rius"
-            ],
             "Deutsch": [
                 "Gegner"
+            ],
+            "Latein": [
+                "advers\u0101rius (advers\u0101ria, advers\u0101rium)"
             ]
         },
         {
-            "Latein": [
-                "\u0101mittere"
-            ],
             "Deutsch": [
                 "verlieren"
+            ],
+            "Latein": [
+                "\u0101mittere (\u0101mitt\u014d, \u0101m\u012bs\u012b, \u0101missum)"
             ]
         },
         {
-            "Latein": [
-                "arma"
-            ],
             "Deutsch": [
                 "Waffen"
+            ],
+            "Latein": [
+                "arma (arm\u014drum n.pl.)"
             ]
         },
         {
-            "Latein": [
-                "autem"
-            ],
             "Deutsch": [
                 "aber"
+            ],
+            "Latein": [
+                "autem"
             ]
         },
         {
-            "Latein": [
-                "corpus"
-            ],
             "Deutsch": [
                 "K\u00f6rper"
+            ],
+            "Latein": [
+                "corpus (corporis n.)"
             ]
         },
         {
-            "Latein": [
-                "cum"
-            ],
             "Deutsch": [
                 "mit"
+            ],
+            "Latein": [
+                "cum (mit Abl.)"
             ]
         },
         {
-            "Latein": [
-                "dolus"
-            ],
             "Deutsch": [
                 "List"
+            ],
+            "Latein": [
+                "dolus (dol\u012b m.)"
             ]
         },
         {
-            "Latein": [
-                "\u0113"
-            ],
             "Deutsch": [
                 "aus etw. heraus, von etw. her"
+            ],
+            "Latein": [
+                "\u0113 (mit Abl.)"
             ]
         },
         {
-            "Latein": [
-                "ex"
-            ],
             "Deutsch": [
                 "aus etw. heraus, von etw. her"
+            ],
+            "Latein": [
+                "ex (mit Abl.)"
             ]
         },
         {
-            "Latein": [
-                "gaud\u0113re"
-            ],
             "Deutsch": [
                 "sich (\u00fcber etw.) freuen"
+            ],
+            "Latein": [
+                "gaud\u0113re (gaude\u014d, g\u0101v\u012bsus sum)"
             ]
         },
         {
-            "Latein": [
-                "gladius"
-            ],
             "Deutsch": [
                 "Schwert"
+            ],
+            "Latein": [
+                "gladius (gladi\u012b m.)"
             ]
         },
         {
-            "Latein": [
-                "hodi\u0113"
-            ],
             "Deutsch": [
                 "heute"
+            ],
+            "Latein": [
+                "hodi\u0113"
             ]
         },
         {
-            "Latein": [
-                "h\u014dra"
-            ],
             "Deutsch": [
                 "Stunde"
+            ],
+            "Latein": [
+                "h\u014dra (h\u014drae f.)"
             ]
         },
         {
-            "Latein": [
-                "iac\u0113re"
-            ],
             "Deutsch": [
                 "liegen"
+            ],
+            "Latein": [
+                "iac\u0113re (iace\u014d, iacu\u012b)"
             ]
         },
         {
-            "Latein": [
-                "in"
-            ],
             "Deutsch": [
                 "in etw., an, auf, bei"
+            ],
+            "Latein": [
+                "in (mit Abl.)"
             ]
         },
         {
-            "Latein": [
-                "incipere"
-            ],
             "Deutsch": [
                 "anfangen"
+            ],
+            "Latein": [
+                "incipere (incipi\u014d, coep\u012b, inceptum)"
             ]
         },
         {
-            "Latein": [
-                "petere"
-            ],
             "Deutsch": [
                 "(anpeilen) aufsuchen, sich begeben, verlangen, (er)bitten, angreifen"
+            ],
+            "Latein": [
+                "petere (pet\u014d, pet\u012bv\u012b, pet\u012btum)"
             ]
         },
         {
-            "Latein": [
-                "populus"
-            ],
             "Deutsch": [
                 "Volk"
+            ],
+            "Latein": [
+                "populus (popul\u012b m.)"
             ]
         },
         {
-            "Latein": [
-                "p\u016bgna"
-            ],
             "Deutsch": [
                 "Kampf, Schlacht"
+            ],
+            "Latein": [
+                "p\u016bgna (p\u016bgnae f.)"
             ]
         },
         {
-            "Latein": [
-                "p\u016bgn\u0101re"
-            ],
             "Deutsch": [
                 "k\u00e4mpfen"
+            ],
+            "Latein": [
+                "p\u016bgn\u0101re (p\u016bgn\u014d, p\u016bgn\u0101v\u012b, p\u016bgn\u0101tum)"
             ]
         },
         {
+            "Deutsch": [
+                "weil"
+            ],
             "Latein": [
                 "quia"
-            ],
-            "Deutsch": [
-                "weil"
             ]
         },
         {
-            "Latein": [
-                "resistere"
-            ],
             "Deutsch": [
                 "stehen bleiben, Widerstand leisten"
+            ],
+            "Latein": [
+                "resistere (resist\u014d, restit\u012b)"
             ]
         },
         {
-            "Latein": [
-                "sal\u016bt\u0101re"
-            ],
             "Deutsch": [
                 "gr\u00fcssen"
+            ],
+            "Latein": [
+                "sal\u016bt\u0101re (sal\u016bt\u014d, sal\u016bt\u0101v\u012b, sal\u016bt\u0101tum)"
             ]
         },
         {
-            "Latein": [
-                "s\u012bgnum"
-            ],
             "Deutsch": [
                 "Zeichen, Feldzeichen, Statue"
+            ],
+            "Latein": [
+                "s\u012bgnum (s\u012bgn\u012b n.)"
             ]
         },
         {
-            "Latein": [
-                "t\u014dtus"
-            ],
             "Deutsch": [
                 "ganz, gesamt"
+            ],
+            "Latein": [
+                "t\u014dtus (t\u014dta, t\u014dtum)"
             ]
         },
         {
-            "Latein": [
-                "v\u012bs"
-            ],
             "Deutsch": [
                 "Kraft, Gewalt, Pl.: Streitkr\u00e4fte"
+            ],
+            "Latein": [
+                "v\u012bs (vim, v\u012b f.)"
             ]
         },
         {
-            "Latein": [
-                "v\u012bta"
-            ],
             "Deutsch": [
                 "Leben"
+            ],
+            "Latein": [
+                "v\u012bta (v\u012btae f.)"
             ]
         },
         {
-            "Latein": [
-                "v\u014dx"
-            ],
             "Deutsch": [
                 "Stimme, Wort, \u00c4usserung"
+            ],
+            "Latein": [
+                "v\u014dx (v\u014dcis f.)"
             ]
         },
         {
-            "Latein": [
-                "cl\u0101rus"
-            ],
             "Deutsch": [
                 "hell, strahlend, ber\u00fchmt"
+            ],
+            "Latein": [
+                "cl\u0101rus (cl\u0101ra, cl\u0101rum)"
             ]
         },
         {
-            "Latein": [
-                "d\u0113"
-            ],
             "Deutsch": [
                 "von etw. herab, von etw. weg, \u00fcber etw."
+            ],
+            "Latein": [
+                "d\u0113 (mit Abl.)"
             ]
         },
         {
-            "Latein": [
-                "d\u0113lect\u0101re"
-            ],
             "Deutsch": [
                 "erfreuen, jmdm. Spass machen"
+            ],
+            "Latein": [
+                "d\u0113lect\u0101re (d\u0113lect\u014d, d\u0113lect\u0101v\u012b, d\u0113lect\u0101tum)"
             ]
         },
         {
-            "Latein": [
-                "eg\u014d"
-            ],
             "Deutsch": [
                 "ich"
+            ],
+            "Latein": [
+                "eg\u014d"
             ]
         },
         {
-            "Latein": [
-                "etiams\u012b"
-            ],
             "Deutsch": [
                 "auch wenn"
+            ],
+            "Latein": [
+                "etiams\u012b"
             ]
         },
         {
-            "Latein": [
-                "fortasse"
-            ],
             "Deutsch": [
                 "vielleicht"
+            ],
+            "Latein": [
+                "fortasse"
             ]
         },
         {
-            "Latein": [
-                "impr\u012bm\u012bs"
-            ],
             "Deutsch": [
                 "vor allem"
+            ],
+            "Latein": [
+                "impr\u012bm\u012bs"
             ]
         },
         {
-            "Latein": [
-                "iterum"
-            ],
             "Deutsch": [
                 "wiederum, noch einmal"
+            ],
+            "Latein": [
+                "iterum"
             ]
         },
         {
-            "Latein": [
-                "l\u016bdus"
-            ],
             "Deutsch": [
                 "Spiel, Wettkampf, Schule"
+            ],
+            "Latein": [
+                "l\u016bdus (l\u016bd\u012b m.)"
             ]
         },
         {
-            "Latein": [
-                "mar\u012btus"
-            ],
             "Deutsch": [
                 "Ehemann"
+            ],
+            "Latein": [
+                "mar\u012btus (mar\u012bt\u012b m.)"
             ]
         },
         {
-            "Latein": [
-                "non s\u014dlum ... sed etiam"
-            ],
             "Deutsch": [
-                "nicht nur ... sondern auch"
+                "nicht nur \u2026 sondern auch"
+            ],
+            "Latein": [
+                "non s\u014dlum \u2026 sed etiam"
             ]
         },
         {
-            "Latein": [
-                "n\u014dnne"
-            ],
             "Deutsch": [
                 "etwa nicht? (man erwartet die Antwort: doch)"
+            ],
+            "Latein": [
+                "n\u014dnne"
             ]
         },
         {
-            "Latein": [
-                "n\u014ds"
-            ],
             "Deutsch": [
                 "wir"
+            ],
+            "Latein": [
+                "n\u014ds"
             ]
         },
         {
-            "Latein": [
-                "n\u016bllus"
-            ],
             "Deutsch": [
                 "kein, keiner"
+            ],
+            "Latein": [
+                "n\u016bllus (n\u016blla, n\u016bllum)"
             ]
         },
         {
-            "Latein": [
-                "num"
-            ],
             "Deutsch": [
                 "denn, etwa? (man erwartet die Antwort: nein)"
+            ],
+            "Latein": [
+                "num"
             ]
         },
         {
-            "Latein": [
-                "po\u0113ta"
-            ],
             "Deutsch": [
                 "Dichter"
+            ],
+            "Latein": [
+                "po\u0113ta (po\u0113tae m.)"
             ]
         },
         {
-            "Latein": [
-                "quod"
-            ],
             "Deutsch": [
                 "weil"
+            ],
+            "Latein": [
+                "quod"
             ]
         },
         {
-            "Latein": [
-                "sacrificium"
-            ],
             "Deutsch": [
                 "Opfer"
+            ],
+            "Latein": [
+                "sacrificium (sacrifici\u012b n.)"
             ]
         },
         {
-            "Latein": [
-                "salv\u0113te!"
-            ],
             "Deutsch": [
                 "Seid gegr\u00fcsst! Guten Tag!"
+            ],
+            "Latein": [
+                "salv\u0113te!"
             ]
         },
         {
-            "Latein": [
-                "tam"
-            ],
             "Deutsch": [
                 "so"
+            ],
+            "Latein": [
+                "tam"
             ]
         },
         {
-            "Latein": [
-                "t\u016b"
-            ],
             "Deutsch": [
                 "du"
+            ],
+            "Latein": [
+                "t\u016b"
             ]
         },
         {
-            "Latein": [
-                "uxor"
-            ],
             "Deutsch": [
                 "Ehefrau"
+            ],
+            "Latein": [
+                "uxor (ux\u014dris f.)"
             ]
         },
         {
-            "Latein": [
-                "v\u014ds"
-            ],
             "Deutsch": [
                 "ihr"
+            ],
+            "Latein": [
+                "v\u014ds"
             ]
         }
     ],
@@ -430,7 +430,7 @@ var liste = {
         "Lernhilfe"
     ],
     "titel": "latein_viva_5-6",
-    "frage_attribut": "Deutsch"
+    "frage_attribut": "Latein"
 };
 
 erste_aufgabe();
