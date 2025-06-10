80be187479ced1c50e31b10fd59777ef32378ab1
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2025-06-10 18:09:02 +0200

    Add Latein 6

---
+++ abfragen/listen/g2023k/Latein_VIVA_Lektion_6.js
@@ -0,0 +1,196 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "cl\u0101rus"
+            ],
+            "Deutsch": [
+                "hell, strahlend, ber\u00fchmt"
+            ]
+        },
+        {
+            "Latein": [
+                "d\u0113"
+            ],
+            "Deutsch": [
+                "von etw. herab, von etw. weg, \u00fcber etw."
+            ]
+        },
+        {
+            "Latein": [
+                "d\u0113lect\u0101re"
+            ],
+            "Deutsch": [
+                "erfreuen, jmdm. Spass machen"
+            ]
+        },
+        {
+            "Latein": [
+                "eg\u014d"
+            ],
+            "Deutsch": [
+                "ich"
+            ]
+        },
+        {
+            "Latein": [
+                "etiams\u012b"
+            ],
+            "Deutsch": [
+                "auch wenn"
+            ]
+        },
+        {
+            "Latein": [
+                "fortasse"
+            ],
+            "Deutsch": [
+                "vielleicht"
+            ]
+        },
+        {
+            "Latein": [
+                "impr\u012bm\u012bs"
+            ],
+            "Deutsch": [
+                "vor allem"
+            ]
+        },
+        {
+            "Latein": [
+                "iterum"
+            ],
+            "Deutsch": [
+                "wiederum, noch einmal"
+            ]
+        },
+        {
+            "Latein": [
+                "l\u016bdus"
+            ],
+            "Deutsch": [
+                "Spiel, Wettkampf, Schule"
+            ]
+        },
+        {
+            "Latein": [
+                "mar\u012btus"
+            ],
+            "Deutsch": [
+                "Ehemann"
+            ]
+        },
+        {
+            "Latein": [
+                "non s\u014dlum \u2026 sed etiam"
+            ],
+            "Deutsch": [
+                "nicht nur \u2026 sondern auch"
+            ]
+        },
+        {
+            "Latein": [
+                "n\u014dnne"
+            ],
+            "Deutsch": [
+                "etwa nicht? (man erwartet die Antwort: doch)"
+            ]
+        },
+        {
+            "Latein": [
+                "n\u014ds"
+            ],
+            "Deutsch": [
+                "wir"
+            ]
+        },
+        {
+            "Latein": [
+                "n\u016bllus"
+            ],
+            "Deutsch": [
+                "kein, keiner"
+            ]
+        },
+        {
+            "Latein": [
+                "num"
+            ],
+            "Deutsch": [
+                "denn, etwa? (man erwartet die Antwort: nein)"
+            ]
+        },
+        {
+            "Latein": [
+                "po\u0113ta"
+            ],
+            "Deutsch": [
+                "Dichter"
+            ]
+        },
+        {
+            "Latein": [
+                "quod"
+            ],
+            "Deutsch": [
+                "weil"
+            ]
+        },
+        {
+            "Latein": [
+                "sacrificium"
+            ],
+            "Deutsch": [
+                "Opfer"
+            ]
+        },
+        {
+            "Latein": [
+                "salv\u0113te!"
+            ],
+            "Deutsch": [
+                "Seid gegr\u00fcsst! Guten Tag!"
+            ]
+        },
+        {
+            "Latein": [
+                "tam"
+            ],
+            "Deutsch": [
+                "so"
+            ]
+        },
+        {
+            "Latein": [
+                "t\u016b"
+            ],
+            "Deutsch": [
+                "du"
+            ]
+        },
+        {
+            "Latein": [
+                "uxor"
+            ],
+            "Deutsch": [
+                "Ehefrau"
+            ]
+        },
+        {
+            "Latein": [
+                "v\u014ds"
+            ],
+            "Deutsch": [
+                "ihr"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Latein_VIVA_Lektion_6",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
