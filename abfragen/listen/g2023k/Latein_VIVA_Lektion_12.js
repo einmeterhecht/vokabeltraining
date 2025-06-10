fc9d6509af47dde8effa65f0af18059e5afb02e9
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2025-06-10 18:11:14 +0200

    Add Latein 12

---
+++ abfragen/listen/g2023k/Latein_VIVA_Lektion_12.js
@@ -0,0 +1,180 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "aedific\u0101re"
+            ],
+            "Deutsch": [
+                "bauen"
+            ]
+        },
+        {
+            "Latein": [
+                "am\u012bcus"
+            ],
+            "Deutsch": [
+                "Freund"
+            ]
+        },
+        {
+            "Latein": [
+                "ant\u012bquus"
+            ],
+            "Deutsch": [
+                "alt"
+            ]
+        },
+        {
+            "Latein": [
+                "bellum"
+            ],
+            "Deutsch": [
+                "Krieg"
+            ]
+        },
+        {
+            "Latein": [
+                "contr\u0101"
+            ],
+            "Deutsch": [
+                "gegen"
+            ]
+        },
+        {
+            "Latein": [
+                "d\u0113l\u012bber\u0101re"
+            ],
+            "Deutsch": [
+                "\u00fcberlegen"
+            ]
+        },
+        {
+            "Latein": [
+                "d\u016bcere"
+            ],
+            "Deutsch": [
+                "f\u00fchren, meinen, f\u00fcr etw. halten"
+            ]
+        },
+        {
+            "Latein": [
+                "f\u012bnis"
+            ],
+            "Deutsch": [
+                "Grenze (im Pl. auch Gebiet), Ende, Ziel, Zweck"
+            ]
+        },
+        {
+            "Latein": [
+                "g\u0113ns"
+            ],
+            "Deutsch": [
+                "(vornehme) Familie, Geschlecht, Volk, Stamm"
+            ]
+        },
+        {
+            "Latein": [
+                "inv\u012bt\u0101re"
+            ],
+            "Deutsch": [
+                "einladen"
+            ]
+        },
+        {
+            "Latein": [
+                "multum"
+            ],
+            "Deutsch": [
+                "viel, sehr, oft"
+            ]
+        },
+        {
+            "Latein": [
+                "par\u0101re"
+            ],
+            "Deutsch": [
+                "(vor)bereiten"
+            ]
+        },
+        {
+            "Latein": [
+                "postquam"
+            ],
+            "Deutsch": [
+                "nachdem"
+            ]
+        },
+        {
+            "Latein": [
+                "rapere"
+            ],
+            "Deutsch": [
+                "rauben, (weg)reissen"
+            ]
+        },
+        {
+            "Latein": [
+                "sine"
+            ],
+            "Deutsch": [
+                "ohne"
+            ]
+        },
+        {
+            "Latein": [
+                "s\u014dlus"
+            ],
+            "Deutsch": [
+                "allein"
+            ]
+        },
+        {
+            "Latein": [
+                "tamquam"
+            ],
+            "Deutsch": [
+                "wie"
+            ]
+        },
+        {
+            "Latein": [
+                "\u016bnus"
+            ],
+            "Deutsch": [
+                "ein (einziger), einzigartig"
+            ]
+        },
+        {
+            "Latein": [
+                "urbs"
+            ],
+            "Deutsch": [
+                "(sehr bedeutende) Stadt, Rom"
+            ]
+        },
+        {
+            "Latein": [
+                "v\u012bvere"
+            ],
+            "Deutsch": [
+                "leben"
+            ]
+        },
+        {
+            "Latein": [
+                "vol\u0101re"
+            ],
+            "Deutsch": [
+                "fliegen"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Latein_VIVA_Lektion_12",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
