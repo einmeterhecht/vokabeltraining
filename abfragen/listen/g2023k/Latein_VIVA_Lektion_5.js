259db8dee1d939ca20c0f59ce8988df3810502a6
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2025-06-10 17:57:45 +0200

    Add Latein 5

---
+++ abfragen/listen/g2023k/Latein_VIVA_Lektion_5.js
@@ -0,0 +1,244 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "\u0101"
+            ],
+            "Deutsch": [
+                "von, von etw. her"
+            ]
+        },
+        {
+            "Latein": [
+                "ab"
+            ],
+            "Deutsch": [
+                "von, von etw. her"
+            ]
+        },
+        {
+            "Latein": [
+                "advers\u0101rius"
+            ],
+            "Deutsch": [
+                "Gegner"
+            ]
+        },
+        {
+            "Latein": [
+                "\u0101mittere"
+            ],
+            "Deutsch": [
+                "verlieren"
+            ]
+        },
+        {
+            "Latein": [
+                "arma"
+            ],
+            "Deutsch": [
+                "Waffen"
+            ]
+        },
+        {
+            "Latein": [
+                "autem"
+            ],
+            "Deutsch": [
+                "aber"
+            ]
+        },
+        {
+            "Latein": [
+                "corpus"
+            ],
+            "Deutsch": [
+                "K\u00f6rper"
+            ]
+        },
+        {
+            "Latein": [
+                "cum"
+            ],
+            "Deutsch": [
+                "mit"
+            ]
+        },
+        {
+            "Latein": [
+                "dolus"
+            ],
+            "Deutsch": [
+                "List"
+            ]
+        },
+        {
+            "Latein": [
+                "\u0113"
+            ],
+            "Deutsch": [
+                "aus etw. heraus, von etw. her"
+            ]
+        },
+        {
+            "Latein": [
+                "ex"
+            ],
+            "Deutsch": [
+                "aus etw. heraus, von etw. her"
+            ]
+        },
+        {
+            "Latein": [
+                "gaud\u0113re"
+            ],
+            "Deutsch": [
+                "sich (\u00fcber etw.) freuen"
+            ]
+        },
+        {
+            "Latein": [
+                "gladius"
+            ],
+            "Deutsch": [
+                "Schwert"
+            ]
+        },
+        {
+            "Latein": [
+                "hodi\u0113"
+            ],
+            "Deutsch": [
+                "heute"
+            ]
+        },
+        {
+            "Latein": [
+                "h\u014dra"
+            ],
+            "Deutsch": [
+                "Stunde"
+            ]
+        },
+        {
+            "Latein": [
+                "iac\u0113re"
+            ],
+            "Deutsch": [
+                "liegen"
+            ]
+        },
+        {
+            "Latein": [
+                "in"
+            ],
+            "Deutsch": [
+                "in etw., an, auf, bei"
+            ]
+        },
+        {
+            "Latein": [
+                "incipere"
+            ],
+            "Deutsch": [
+                "anfangen"
+            ]
+        },
+        {
+            "Latein": [
+                "petere"
+            ],
+            "Deutsch": [
+                "(anpeilen) aufsuchen, sich begeben, verlangen, (er)bitten, angreifen"
+            ]
+        },
+        {
+            "Latein": [
+                "populus"
+            ],
+            "Deutsch": [
+                "Volk"
+            ]
+        },
+        {
+            "Latein": [
+                "p\u016bgna"
+            ],
+            "Deutsch": [
+                "Kampf, Schlacht"
+            ]
+        },
+        {
+            "Latein": [
+                "p\u016bgn\u0101re"
+            ],
+            "Deutsch": [
+                "k\u00e4mpfen"
+            ]
+        },
+        {
+            "Latein": [
+                "quia"
+            ],
+            "Deutsch": [
+                "weil"
+            ]
+        },
+        {
+            "Latein": [
+                "resistere"
+            ],
+            "Deutsch": [
+                "stehen bleiben, Widerstand leisten"
+            ]
+        },
+        {
+            "Latein": [
+                "sal\u016bt\u0101re"
+            ],
+            "Deutsch": [
+                "gr\u00fcssen"
+            ]
+        },
+        {
+            "Latein": [
+                "s\u012bgnum"
+            ],
+            "Deutsch": [
+                "Zeichen, Feldzeichen, Statue"
+            ]
+        },
+        {
+            "Latein": [
+                "t\u014dtus"
+            ],
+            "Deutsch": [
+                "ganz, gesamt"
+            ]
+        },
+        {
+            "Latein": [
+                "v\u012bs"
+            ],
+            "Deutsch": [
+                "Kraft, Gewalt, Pl.: Streitkr\u00e4fte"
+            ]
+        },
+        {
+            "Latein": [
+                "v\u012bta"
+            ],
+            "Deutsch": [
+                "Leben"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Latein_VIVA_Lektion_5",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
