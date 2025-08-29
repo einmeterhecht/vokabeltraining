9ae68fc213ba81e437058f7fa4bed2fbb026dea0
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2025-08-29 14:21:41 +0200

    Add latein voci

---
+++ abfragen/listen/latein/VIVA_18.js
@@ -0,0 +1,228 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "\u0101cer"
+            ],
+            "Deutsch": [
+                "scharf, heftig"
+            ]
+        },
+        {
+            "Latein": [
+                "apud"
+            ],
+            "Deutsch": [
+                "bei"
+            ]
+        },
+        {
+            "Latein": [
+                "asinus"
+            ],
+            "Deutsch": [
+                "Esel"
+            ]
+        },
+        {
+            "Latein": [
+                "discere"
+            ],
+            "Deutsch": [
+                "lernen, erfahren"
+            ]
+        },
+        {
+            "Latein": [
+                "discipulus"
+            ],
+            "Deutsch": [
+                "Sch\u00fcler"
+            ]
+        },
+        {
+            "Latein": [
+                "doctus"
+            ],
+            "Deutsch": [
+                "gelehrt, gebildet"
+            ]
+        },
+        {
+            "Latein": [
+                "dulcis"
+            ],
+            "Deutsch": [
+                "s\u00fcss, angenehm"
+            ]
+        },
+        {
+            "Latein": [
+                "dum"
+            ],
+            "Deutsch": [
+                "w\u00e4hrend"
+            ]
+        },
+        {
+            "Latein": [
+                "\u0113leg\u0101ns"
+            ],
+            "Deutsch": [
+                "geschmackvoll"
+            ]
+        },
+        {
+            "Latein": [
+                "\u0113loquentia"
+            ],
+            "Deutsch": [
+                "Beredsamkeit"
+            ]
+        },
+        {
+            "Latein": [
+                "h\u016bm\u0101nit\u0101s"
+            ],
+            "Deutsch": [
+                "Menschlichkeit, Bildung"
+            ]
+        },
+        {
+            "Latein": [
+                "\u012bnsula"
+            ],
+            "Deutsch": [
+                "Insel, Wohnblock"
+            ]
+        },
+        {
+            "Latein": [
+                "laud\u0101re"
+            ],
+            "Deutsch": [
+                "loben"
+            ]
+        },
+        {
+            "Latein": [
+                "locus"
+            ],
+            "Deutsch": [
+                "Ort"
+            ]
+        },
+        {
+            "Latein": [
+                "melior"
+            ],
+            "Deutsch": [
+                "besser"
+            ]
+        },
+        {
+            "Latein": [
+                "n\u014dbilis"
+            ],
+            "Deutsch": [
+                "ber\u00fchmt, adlig"
+            ]
+        },
+        {
+            "Latein": [
+                "omnis"
+            ],
+            "Deutsch": [
+                "jeder, ganz, alle"
+            ]
+        },
+        {
+            "Latein": [
+                "\u014dr\u0101ti\u014d"
+            ],
+            "Deutsch": [
+                "Rede"
+            ]
+        },
+        {
+            "Latein": [
+                "p\u0101r"
+            ],
+            "Deutsch": [
+                "gleich"
+            ]
+        },
+        {
+            "Latein": [
+                "praedic\u0101re"
+            ],
+            "Deutsch": [
+                "laut verk\u00fcnden, r\u00fchmen"
+            ]
+        },
+        {
+            "Latein": [
+                "s\u0113 d\u0113dere"
+            ],
+            "Deutsch": [
+                "sich jmdm. ausliefern, sich einer Sache widmen"
+            ]
+        },
+        {
+            "Latein": [
+                "simul"
+            ],
+            "Deutsch": [
+                "zugleich, gleichzeitig"
+            ]
+        },
+        {
+            "Latein": [
+                "singul\u0101ris"
+            ],
+            "Deutsch": [
+                "einzeln, einzigartig"
+            ]
+        },
+        {
+            "Latein": [
+                "somnus"
+            ],
+            "Deutsch": [
+                "Schlaf"
+            ]
+        },
+        {
+            "Latein": [
+                "studium"
+            ],
+            "Deutsch": [
+                "Eifer, Interesse, Besch\u00e4ftigung"
+            ]
+        },
+        {
+            "Latein": [
+                "tollere"
+            ],
+            "Deutsch": [
+                "aufheben: hochheben, aufheben: beseitigen"
+            ]
+        },
+        {
+            "Latein": [
+                "ultimus"
+            ],
+            "Deutsch": [
+                "der letzte, der \u00e4usserste"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "VIVA_18",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
