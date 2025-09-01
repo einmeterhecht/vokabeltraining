ed110eda1583702e33e7be48ac3f8e879b8b2e61
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2025-09-01 18:22:12 +0200

    Add viva 18+

---
+++ abfragen/listen/latein/VIVA_18+.js
@@ -0,0 +1,236 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "nobilis, e"
+            ],
+            "Deutsch": [
+                "ber\u00fchmt, adlig, edel"
+            ]
+        },
+        {
+            "Latein": [
+                "locus, -i m."
+            ],
+            "Deutsch": [
+                "Ort"
+            ]
+        },
+        {
+            "Latein": [
+                "insula, -ae f."
+            ],
+            "Deutsch": [
+                "1. Insel2. Wohnblock"
+            ]
+        },
+        {
+            "Latein": [
+                "dulcis, e"
+            ],
+            "Deutsch": [
+                "s\u00fcss, angenehm"
+            ]
+        },
+        {
+            "Latein": [
+                "apud (+ Akk.)"
+            ],
+            "Deutsch": [
+                "bei"
+            ]
+        },
+        {
+            "Latein": [
+                "eloquentia, -ae f."
+            ],
+            "Deutsch": [
+                "Beredsamkeit, Redegewandtheit"
+            ]
+        },
+        {
+            "Latein": [
+                "melior, melius (Gen. melioris)"
+            ],
+            "Deutsch": [
+                "besser"
+            ]
+        },
+        {
+            "Latein": [
+                "se dedere, dedo, dedidi, deditum (+ Dat.)"
+            ],
+            "Deutsch": [
+                "sich jdm. ausliefern, sich einer Sache widmen"
+            ]
+        },
+        {
+            "Latein": [
+                "discere, disco, didici, --"
+            ],
+            "Deutsch": [
+                "lernen, erfahren"
+            ]
+        },
+        {
+            "Latein": [
+                "somnus, -i m."
+            ],
+            "Deutsch": [
+                "Schlaf"
+            ]
+        },
+        {
+            "Latein": [
+                "dum (+ Ind.Pr\u00e4s.)"
+            ],
+            "Deutsch": [
+                "w\u00e4hrend"
+            ]
+        },
+        {
+            "Latein": [
+                "omnis, -ePl.: omnes, omnium"
+            ],
+            "Deutsch": [
+                "jeder, ganz im Pl.: alle, alles"
+            ]
+        },
+        {
+            "Latein": [
+                "discipulus, -i m.discipula, -ae f."
+            ],
+            "Deutsch": [
+                "Sch\u00fclerSch\u00fclerin"
+            ]
+        },
+        {
+            "Latein": [
+                "acer, acris, acre"
+            ],
+            "Deutsch": [
+                "scharf, heftig"
+            ]
+        },
+        {
+            "Latein": [
+                "oratio, orationis f."
+            ],
+            "Deutsch": [
+                "Rede"
+            ]
+        },
+        {
+            "Latein": [
+                "laus, laudis f."
+            ],
+            "Deutsch": [
+                "Lob, Ruhm"
+            ]
+        },
+        {
+            "Latein": [
+                "doctus, a, um"
+            ],
+            "Deutsch": [
+                "gelehrt, gebildet"
+            ]
+        },
+        {
+            "Latein": [
+                "elegans, elegantis"
+            ],
+            "Deutsch": [
+                "geschmackvoll(\"auserlesen\")"
+            ]
+        },
+        {
+            "Latein": [
+                "singularis, e"
+            ],
+            "Deutsch": [
+                "einzeln, einzigartig"
+            ]
+        },
+        {
+            "Latein": [
+                "tollere, tollo, sustuli, sublatum"
+            ],
+            "Deutsch": [
+                "1. aufheben, hochheben2. aufheben, beseitigen"
+            ]
+        },
+        {
+            "Latein": [
+                "laudare"
+            ],
+            "Deutsch": [
+                "loben"
+            ]
+        },
+        {
+            "Latein": [
+                "simul (Adv.)"
+            ],
+            "Deutsch": [
+                "zugleich, gleichzeitig"
+            ]
+        },
+        {
+            "Latein": [
+                "praedicare"
+            ],
+            "Deutsch": [
+                "laut verk\u00fcnden, r\u00fchmen"
+            ]
+        },
+        {
+            "Latein": [
+                "ultimus, a, um"
+            ],
+            "Deutsch": [
+                "der letzte, der \u00e4usserste"
+            ]
+        },
+        {
+            "Latein": [
+                "humanitas, humanitatis f."
+            ],
+            "Deutsch": [
+                "Menschlichkeit, Bildung"
+            ]
+        },
+        {
+            "Latein": [
+                "par, paris"
+            ],
+            "Deutsch": [
+                "gleich"
+            ]
+        },
+        {
+            "Latein": [
+                "par atque/ac"
+            ],
+            "Deutsch": [
+                "gleich wie"
+            ]
+        },
+        {
+            "Latein": [
+                "studium, -i n."
+            ],
+            "Deutsch": [
+                "Eifer, Interesse, Besch\u00e4ftigung"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "VIVA_18+",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
