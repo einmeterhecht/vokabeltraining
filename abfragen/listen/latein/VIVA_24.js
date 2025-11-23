eb0a40f7c8ebde60fbefb5be0777e24d5c3dd456
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2025-11-23 17:27:38 +0100

    Add Latein24

---
+++ abfragen/listen/latein/VIVA_24.js
@@ -0,0 +1,212 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "meminisse, memin\u012b (+ Gen./Akk.)"
+            ],
+            "Deutsch": [
+                "sich erinnern an (im Dt. Pr\u00e4sens, im Lat. Perfektformen!)"
+            ]
+        },
+        {
+            "Latein": [
+                "exercitus, -\u016bs m."
+            ],
+            "Deutsch": [
+                "Heer"
+            ]
+        },
+        {
+            "Latein": [
+                "mittere, mitt\u014d, m\u012bs\u012b, missum 3k"
+            ],
+            "Deutsch": [
+                "schicken"
+            ]
+        },
+        {
+            "Latein": [
+                "manus, -\u016bs f."
+            ],
+            "Deutsch": [
+                "1. Hand 2. Gruppe"
+            ]
+        },
+        {
+            "Latein": [
+                "aud\u0101x, aud\u0101cis"
+            ],
+            "Deutsch": [
+                "k\u00fchn:1. frech 2. mutig"
+            ]
+        },
+        {
+            "Latein": [
+                "praeesse, praesum, praefu\u012b (+ Dat.)"
+            ],
+            "Deutsch": [
+                "an der Spitze stehen, jdn. kommandieren, etw. verwalten"
+            ]
+        },
+        {
+            "Latein": [
+                "impetus, -\u016bs m."
+            ],
+            "Deutsch": [
+                "Angriff, Schwung"
+            ]
+        },
+        {
+            "Latein": [
+                "d\u0113fendere, d\u0113fend\u014d, d\u0113fend\u012b, d\u0113f\u0113nsum 3k"
+            ],
+            "Deutsch": [
+                "verteidigen, abwehren"
+            ]
+        },
+        {
+            "Latein": [
+                "pellere, pell\u014d, pepul\u012b, pulsum 3k"
+            ],
+            "Deutsch": [
+                "1. stossen, schlagen 2. vertreiben"
+            ]
+        },
+        {
+            "Latein": [
+                "r\u0113s, re\u012b f."
+            ],
+            "Deutsch": [
+                "1. Sache, Ding 2. Angelegenheit"
+            ]
+        },
+        {
+            "Latein": [
+                "asper, aspera, asperum"
+            ],
+            "Deutsch": [
+                "rau, streng"
+            ]
+        },
+        {
+            "Latein": [
+                "v\u0101stus, -a, -um"
+            ],
+            "Deutsch": [
+                "1. ungeheuer weit 2. \u00f6de, w\u00fcst"
+            ]
+        },
+        {
+            "Latein": [
+                "casus, -\u016bs m."
+            ],
+            "Deutsch": [
+                "Fall, Zufall, Ereignis"
+            ]
+        },
+        {
+            "Latein": [
+                "cas\u016b (Adv.)"
+            ],
+            "Deutsch": [
+                "zuf\u00e4lligerweise"
+            ]
+        },
+        {
+            "Latein": [
+                "obsecr\u0101re 1"
+            ],
+            "Deutsch": [
+                "anflehen, beschw\u00f6ren"
+            ]
+        },
+        {
+            "Latein": [
+                "lacus, -\u016bs m."
+            ],
+            "Deutsch": [
+                "See"
+            ]
+        },
+        {
+            "Latein": [
+                "m\u014dnstrum, -\u012b n."
+            ],
+            "Deutsch": [
+                "1. Ungeheuer 2. g\u00f6ttliches Zeichen"
+            ]
+        },
+        {
+            "Latein": [
+                "pernici\u0113s, pernici\u0113\u012b f."
+            ],
+            "Deutsch": [
+                "Verderben, Untergang"
+            ]
+        },
+        {
+            "Latein": [
+                "faci\u0113s, faci\u0113\u012b f."
+            ],
+            "Deutsch": [
+                "1. Gesicht 2. Gestalt"
+            ]
+        },
+        {
+            "Latein": [
+                "sp\u0113s, sp\u0113\u012b f."
+            ],
+            "Deutsch": [
+                "Hoffnung"
+            ]
+        },
+        {
+            "Latein": [
+                "pars, partis f."
+            ],
+            "Deutsch": [
+                "Teil, Seite"
+            ]
+        },
+        {
+            "Latein": [
+                "ferus, -a, -um"
+            ],
+            "Deutsch": [
+                "wild"
+            ]
+        },
+        {
+            "Latein": [
+                "quinque (undekl.)"
+            ],
+            "Deutsch": [
+                "f\u00fcnf"
+            ]
+        },
+        {
+            "Latein": [
+                "di\u0113s, di\u0113\u012b m."
+            ],
+            "Deutsch": [
+                "Tag"
+            ]
+        },
+        {
+            "Latein": [
+                "di\u0113s, di\u0113\u012b f. (!)"
+            ],
+            "Deutsch": [
+                "Termin, Frist"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "VIVA_24",
+    "frage_attribut": "Latein"
+};
+
+erste_aufgabe();
