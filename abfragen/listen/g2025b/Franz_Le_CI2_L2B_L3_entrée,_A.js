d0d592b9a75af2cdc40975d16dc7fab449b0f720
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-03-08 13:45:02 +0100

    Add franz

---
+++ abfragen/listen/g2025b/Franz_Le_CI2_L2B_L3_entrée,_A.js
@@ -0,0 +1,732 @@
+var liste = {
+    "fragen": [
+        {
+            "Franz\u00f6sisch": [
+                "connu, connue"
+            ],
+            "Deutsch": [
+                "bekannt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "personne ne..."
+            ],
+            "Deutsch": [
+                "niemand (Subjekt)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la radio"
+            ],
+            "Deutsch": [
+                "das Radio"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un podcast"
+            ],
+            "Deutsch": [
+                "ein Podcast"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "emball\u00e9, emball\u00e9e"
+            ],
+            "Deutsch": [
+                "begeistert"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ne... personne"
+            ],
+            "Deutsch": [
+                "niemand"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00e9videmment"
+            ],
+            "Deutsch": [
+                "klar, nat\u00fcrlich"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "C'est rasoir."
+            ],
+            "Deutsch": [
+                "Das ist stinklangweilig."
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00e9norm\u00e9ment"
+            ],
+            "Deutsch": [
+                "sehr, unheimlich viel"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une croix"
+            ],
+            "Deutsch": [
+                "ein Kreuz"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un b\u00e2timent"
+            ],
+            "Deutsch": [
+                "ein Geb\u00e4ude"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une r\u00e9sidence"
+            ],
+            "Deutsch": [
+                "ein Wohnsitz, hier: ein Amtssitz"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le Pr\u00e9sident de la R\u00e9publique"
+            ],
+            "Deutsch": [
+                "der Pr\u00e4sident, der Staatspr\u00e4sident"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un employ\u00e9, une employ\u00e9e"
+            ],
+            "Deutsch": [
+                "ein Angestellter, eine Angestellte"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un visiteur, une visiteuse"
+            ],
+            "Deutsch": [
+                "ein Besucher, eine Besucherin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un chef d'\u00c9tat"
+            ],
+            "Deutsch": [
+                "ein Staatschef"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "gentiment"
+            ],
+            "Deutsch": [
+                "nett"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "r\u00e9cemment"
+            ],
+            "Deutsch": [
+                "k\u00fcrzlich, neulich"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "inconnu, inconnue"
+            ],
+            "Deutsch": [
+                "unbekannt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un portrait"
+            ],
+            "Deutsch": [
+                "ein Portr\u00e4t"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "par"
+            ],
+            "Deutsch": [
+                "durch, von"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une tenue"
+            ],
+            "Deutsch": [
+                "eine Kleidung, ein Outfit"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "dessiner"
+            ],
+            "Deutsch": [
+                "etw. zeichnen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "rien ne..."
+            ],
+            "Deutsch": [
+                "nichts (Subjekt)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "changer"
+            ],
+            "Deutsch": [
+                "\u00e4ndern, (sich) ver\u00e4ndern"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un crayon"
+            ],
+            "Deutsch": [
+                "ein Stift"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un endroit"
+            ],
+            "Deutsch": [
+                "ein Ort"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un stade"
+            ],
+            "Deutsch": [
+                "ein Stadion"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le tennis"
+            ],
+            "Deutsch": [
+                "das Tennis"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un court (de tennis)"
+            ],
+            "Deutsch": [
+                "ein Tennisplatz"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "avoir le droit de faire qc"
+            ],
+            "Deutsch": [
+                "das Recht haben, etw. zu tun, etw. tun d\u00fcrfen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "couramment"
+            ],
+            "Deutsch": [
+                "h\u00e4ufig, oft"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "rond, ronde"
+            ],
+            "Deutsch": [
+                "rund"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un studio"
+            ],
+            "Deutsch": [
+                "ein (Aufnahme)Studio"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "assister \u00e0 qc"
+            ],
+            "Deutsch": [
+                "an etw. teilnehmen, etw. miterleben"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ainsi"
+            ],
+            "Deutsch": [
+                "so, auf diese Weise"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un organisateur, une organisatrice"
+            ],
+            "Deutsch": [
+                "ein Organisator, eine Organisatorin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "cacher qc"
+            ],
+            "Deutsch": [
+                "etw. verstecken"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une baguette"
+            ],
+            "Deutsch": [
+                "ein Stab, ein Taktstock"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un meuble"
+            ],
+            "Deutsch": [
+                "ein M\u00f6belst\u00fcck"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le Maroc"
+            ],
+            "Deutsch": [
+                "Marokko"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "m\u00f6glich"
+            ],
+            "Deutsch": [
+                "possible"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "sich mit jdm. austauschen"
+            ],
+            "Deutsch": [
+                "\u00e9changer avec qn"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "regelm\u00e4ssig"
+            ],
+            "Deutsch": [
+                "r\u00e9gulier/r\u00e9guli\u00e8re"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Austausch"
+            ],
+            "Deutsch": [
+                "un \u00e9change"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Postkarte"
+            ],
+            "Deutsch": [
+                "une carte postale"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "der Sommer"
+            ],
+            "Deutsch": [
+                "l'\u00e9t\u00e9 (m.)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "draussen, im Freien"
+            ],
+            "Deutsch": [
+                "dehors"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Felsbucht"
+            ],
+            "Deutsch": [
+                "une calanque"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "der Kajak, das Kajakfahren"
+            ],
+            "Deutsch": [
+                "le kayak"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "von jdm./etw. Fan sein"
+            ],
+            "Deutsch": [
+                "\u00eatre fan de qn/qc"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Abonnement, eine Dauerkarte"
+            ],
+            "Deutsch": [
+                "un abonnement"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Das ist der Wahnsinn."
+            ],
+            "Deutsch": [
+                "C'est la folie."
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Korken, hier: ein Verkehrsstau"
+            ],
+            "Deutsch": [
+                "un bouchon"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Gef\u00e4ngnis"
+            ],
+            "Deutsch": [
+                "une prison"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Freund/eine Freundin, ein Kumpel (ugs.)"
+            ],
+            "Deutsch": [
+                "un pote/une pote"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Gebrauchsanweisung"
+            ],
+            "Deutsch": [
+                "un mode d'emploi"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Marathon"
+            ],
+            "Deutsch": [
+                "un marathon"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Brosch\u00fcre"
+            ],
+            "Deutsch": [
+                "une brochure"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "etw. verpassen, etw. vers\u00e4umen"
+            ],
+            "Deutsch": [
+                "manquer qc"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "nachdenken (\u00fcber), \u00fcberlegen"
+            ],
+            "Deutsch": [
+                "r\u00e9fl\u00e9chir (\u00e0)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "r\u00e9fl\u00e9chir wird konjugiert wie finir."
+            ],
+            "Deutsch": [
+                "je r\u00e9fl\u00e9chistu r\u00e9fl\u00e9chisil/elle/on r\u00e9fl\u00e9chitnous r\u00e9f\u00e9chissonsvous r\u00e9fl\u00e9chissezils/elles r\u00e9fl\u00e9chissentp.c.: j'ai r\u00e9fl\u00e9chi"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Angst haben etw. zu tun"
+            ],
+            "Deutsch": [
+                "avoir peur de faire qc"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "das Heimweh"
+            ],
+            "Deutsch": [
+                "le mal du pays"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Zweifel"
+            ],
+            "Deutsch": [
+                "un doute"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "sofort"
+            ],
+            "Deutsch": [
+                "tout de suite"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "jdn. ermutigen"
+            ],
+            "Deutsch": [
+                "encourager qn"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ich ermutige, wir ermutigen"
+            ],
+            "Deutsch": [
+                "j'encourage, nous encourageons"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Erfahrung"
+            ],
+            "Deutsch": [
+                "une exp\u00e9rience"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "einzig(artig)"
+            ],
+            "Deutsch": [
+                "unique"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Fortschritt"
+            ],
+            "Deutsch": [
+                "un progr\u00e8s"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Kultur"
+            ],
+            "Deutsch": [
+                "une culture"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "schliesslich, zum Schluss"
+            ],
+            "Deutsch": [
+                "finalement"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "sich entscheiden"
+            ],
+            "Deutsch": [
+                "se d\u00e9cider"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Website"
+            ],
+            "Deutsch": [
+                "un site"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "leicht"
+            ],
+            "Deutsch": [
+                "facile"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "sich f\u00fcr etw. interessieren"
+            ],
+            "Deutsch": [
+                "s'int\u00e9resser \u00e0 qc"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "eine Anzeige"
+            ],
+            "Deutsch": [
+                "une annonce"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "mit jdm. chatten"
+            ],
+            "Deutsch": [
+                "chatter avec qn"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "etw. behalten"
+            ],
+            "Deutsch": [
+                "garder qc"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Ordner, eine Akte, hier: Bewerbungsunterlagen"
+            ],
+            "Deutsch": [
+                "un dossier"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "etw. regeln"
+            ],
+            "Deutsch": [
+                "r\u00e9gler qc"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Ostern"
+            ],
+            "Deutsch": [
+                "P\u00e2ques (f., pl.)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Kulturwissen"
+            ],
+            "Deutsch": [
+                "En France, ce sont les cloches (Kirchenglocken) et pas les lapins (Hasen) qui apportent les \u0153ufs \u00e0 P\u00e2ques."
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "hinunterfahren"
+            ],
+            "Deutsch": [
+                "descendre \u00e0 (Ortsname)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ungeduldig"
+            ],
+            "Deutsch": [
+                "impatient/impatiente"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "weggehen"
+            ],
+            "Deutsch": [
+                "s'en aller"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "sich f\u00fchlen"
+            ],
+            "Deutsch": [
+                "se sentir"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Konjugation se sentir = partir"
+            ],
+            "Deutsch": [
+                "je me senstu te sensil/elle se sentnous nous sentonsvous vous sentezils/elles se sententp.c.: je me suis senti(e)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ein Couscous"
+            ],
+            "Deutsch": [
+                "un couscous"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Franz_Le_CI2_L2B_L3_entr\u00e9e,_A",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
