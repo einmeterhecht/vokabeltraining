538dd1ee17b90e7821f75a06770600b42552a151
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-03-21 10:39:05 +0100

    Add english

--- README.md
+++ README.md
@@ -37,6 +37,8 @@ Dieser webbasiertes Vokabeltrainer ist für ein Lernen mit Tastatureingabe optim
 
 ### Für g2025b:
 
+- [English L5, 6](/vokabeltraining/abfragen?folder=g2025b&file=English_L5,_6)
+
 - [Franz Le CI2 L2B entrée, A](/vokabeltraining/abfragen?folder=g2025b&file=Franz_Le_CI2_L2B_L3_entrée,_A)
 
 - [English Li L2&3](/vokabeltraining/abfragen?folder=g2025b&file=English_LI_L2,3)
---
+++ abfragen/listen/g2025b/English_L5,_6.js
@@ -0,0 +1,596 @@
+var liste = {
+    "fragen": [
+        {
+            "English": [
+                "fist"
+            ],
+            "Deutsch": [
+                "die Faust"
+            ]
+        },
+        {
+            "English": [
+                "guide"
+            ],
+            "Deutsch": [
+                "der Leitfaden, die Orientierungshilfe"
+            ]
+        },
+        {
+            "English": [
+                "handful"
+            ],
+            "Deutsch": [
+                "eine Handvoll"
+            ]
+        },
+        {
+            "English": [
+                "palm"
+            ],
+            "Deutsch": [
+                "die Handfl\u00e4che"
+            ]
+        },
+        {
+            "English": [
+                "portion"
+            ],
+            "Deutsch": [
+                "die Portion"
+            ]
+        },
+        {
+            "English": [
+                "squash"
+            ],
+            "Deutsch": [
+                "der K\u00fcrbis"
+            ]
+        },
+        {
+            "English": [
+                "sweet potato"
+            ],
+            "Deutsch": [
+                "die S\u00fc\u00dfkartoffel"
+            ]
+        },
+        {
+            "English": [
+                "ban"
+            ],
+            "Deutsch": [
+                "verbieten, ausschlie\u00dfen, stoppen"
+            ]
+        },
+        {
+            "English": [
+                "crust"
+            ],
+            "Deutsch": [
+                "die Kruste/Rinde"
+            ]
+        },
+        {
+            "English": [
+                "ferment"
+            ],
+            "Deutsch": [
+                "fermentieren, verg\u00e4ren"
+            ]
+        },
+        {
+            "English": [
+                "manuscript"
+            ],
+            "Deutsch": [
+                "das Manuskript"
+            ]
+        },
+        {
+            "English": [
+                "millennium"
+            ],
+            "Deutsch": [
+                "das Jahrtausend"
+            ]
+        },
+        {
+            "English": [
+                "rival"
+            ],
+            "Deutsch": [
+                "der Rivale, die Rivalin"
+            ]
+        },
+        {
+            "English": [
+                "sparkling"
+            ],
+            "Deutsch": [
+                "sprudelnd"
+            ]
+        },
+        {
+            "English": [
+                "spiral"
+            ],
+            "Deutsch": [
+                "die Spirale"
+            ]
+        },
+        {
+            "English": [
+                "status"
+            ],
+            "Deutsch": [
+                "die Status/Rang"
+            ]
+        },
+        {
+            "English": [
+                "suitable"
+            ],
+            "Deutsch": [
+                "geeignet"
+            ]
+        },
+        {
+            "English": [
+                "avoid"
+            ],
+            "Deutsch": [
+                "vermeiden"
+            ]
+        },
+        {
+            "English": [
+                "chew"
+            ],
+            "Deutsch": [
+                "kauen"
+            ]
+        },
+        {
+            "English": [
+                "cut down on"
+            ],
+            "Deutsch": [
+                "k\u00fcrzen, einschr\u00e4nken"
+            ]
+        },
+        {
+            "English": [
+                "cut out"
+            ],
+            "Deutsch": [
+                "weglassen"
+            ]
+        },
+        {
+            "English": [
+                "fatty"
+            ],
+            "Deutsch": [
+                "fetthaltig"
+            ]
+        },
+        {
+            "English": [
+                "give up"
+            ],
+            "Deutsch": [
+                "aufh\u00f6ren, einstellen"
+            ]
+        },
+        {
+            "English": [
+                "junk food"
+            ],
+            "Deutsch": [
+                "das Fastfood"
+            ]
+        },
+        {
+            "English": [
+                "obesity"
+            ],
+            "Deutsch": [
+                "die Fettleibigkeit"
+            ]
+        },
+        {
+            "English": [
+                "overweight"
+            ],
+            "Deutsch": [
+                "das \u00dcbergewicht"
+            ]
+        },
+        {
+            "English": [
+                "take up"
+            ],
+            "Deutsch": [
+                "aufgreifen, anfangen"
+            ]
+        },
+        {
+            "English": [
+                "willpower"
+            ],
+            "Deutsch": [
+                "die Willenskraft"
+            ]
+        },
+        {
+            "English": [
+                "alertness"
+            ],
+            "Deutsch": [
+                "die Aufmerksamkeit"
+            ]
+        },
+        {
+            "English": [
+                "asthma"
+            ],
+            "Deutsch": [
+                "das Asthma"
+            ]
+        },
+        {
+            "English": [
+                "beneficial"
+            ],
+            "Deutsch": [
+                "n\u00fctzlich, vorteilhaft"
+            ]
+        },
+        {
+            "English": [
+                "blood pressure"
+            ],
+            "Deutsch": [
+                "der Blutdruck"
+            ]
+        },
+        {
+            "English": [
+                "disease"
+            ],
+            "Deutsch": [
+                "die Krankheit"
+            ]
+        },
+        {
+            "English": [
+                "habit-forming"
+            ],
+            "Deutsch": [
+                "s\u00fcchtig machend"
+            ]
+        },
+        {
+            "English": [
+                "harmful"
+            ],
+            "Deutsch": [
+                "sch\u00e4dlich"
+            ]
+        },
+        {
+            "English": [
+                "ingredient"
+            ],
+            "Deutsch": [
+                "die Zutat"
+            ]
+        },
+        {
+            "English": [
+                "mood-changing"
+            ],
+            "Deutsch": [
+                "stimmungs-beeinflussend"
+            ]
+        },
+        {
+            "English": [
+                "neuroscientist"
+            ],
+            "Deutsch": [
+                "der/die Neurowissen-schaftler/-in"
+            ]
+        },
+        {
+            "English": [
+                "relieve"
+            ],
+            "Deutsch": [
+                "lindern"
+            ]
+        },
+        {
+            "English": [
+                "shift work"
+            ],
+            "Deutsch": [
+                "die Schichtarbeit"
+            ]
+        },
+        {
+            "English": [
+                "bill"
+            ],
+            "Deutsch": [
+                "die Rechnung"
+            ]
+        },
+        {
+            "English": [
+                "course"
+            ],
+            "Deutsch": [
+                "der Gang"
+            ]
+        },
+        {
+            "English": [
+                "dessert"
+            ],
+            "Deutsch": [
+                "der Nachtisch"
+            ]
+        },
+        {
+            "English": [
+                "fritter"
+            ],
+            "Deutsch": [
+                "in Teig ausge-backenes St\u00fcck Obst/Gem\u00fcse etc."
+            ]
+        },
+        {
+            "English": [
+                "inedible"
+            ],
+            "Deutsch": [
+                "ungenie\u00dfbar"
+            ]
+        },
+        {
+            "English": [
+                "profit"
+            ],
+            "Deutsch": [
+                "der Gewinn"
+            ]
+        },
+        {
+            "English": [
+                "savoury"
+            ],
+            "Deutsch": [
+                "pikant"
+            ]
+        },
+        {
+            "English": [
+                "scrap"
+            ],
+            "Deutsch": [
+                "abschaffen, aussondern"
+            ]
+        },
+        {
+            "English": [
+                "sell-by date"
+            ],
+            "Deutsch": [
+                "das Mindesthalt-barkeitsdatum"
+            ]
+        },
+        {
+            "English": [
+                "soak"
+            ],
+            "Deutsch": [
+                "einweichen, w\u00e4ssern"
+            ]
+        },
+        {
+            "English": [
+                "starter"
+            ],
+            "Deutsch": [
+                "die Vorspeise"
+            ]
+        },
+        {
+            "English": [
+                "tip"
+            ],
+            "Deutsch": [
+                "das Trinkgeld"
+            ]
+        },
+        {
+            "English": [
+                "unsold"
+            ],
+            "Deutsch": [
+                "nicht verkauft"
+            ]
+        },
+        {
+            "English": [
+                "unwanted"
+            ],
+            "Deutsch": [
+                "unerw\u00fcnscht"
+            ]
+        },
+        {
+            "English": [
+                "annually"
+            ],
+            "Deutsch": [
+                "j\u00e4hrlich"
+            ]
+        },
+        {
+            "English": [
+                "breathe in"
+            ],
+            "Deutsch": [
+                "einatmen"
+            ]
+        },
+        {
+            "English": [
+                "crowd"
+            ],
+            "Deutsch": [
+                "sich dr\u00e4ngen"
+            ]
+        },
+        {
+            "English": [
+                "cute"
+            ],
+            "Deutsch": [
+                "niedlich"
+            ]
+        },
+        {
+            "English": [
+                "fool"
+            ],
+            "Deutsch": [
+                "jdm. etw. vormachen"
+            ]
+        },
+        {
+            "English": [
+                "licence"
+            ],
+            "Deutsch": [
+                "die Zulassung/ Fahrerlaubnis/ Lizenz"
+            ]
+        },
+        {
+            "English": [
+                "major"
+            ],
+            "Deutsch": [
+                "bedeutend"
+            ]
+        },
+        {
+            "English": [
+                "poison"
+            ],
+            "Deutsch": [
+                "das Gift"
+            ]
+        },
+        {
+            "English": [
+                "regulation"
+            ],
+            "Deutsch": [
+                "die Vorschrift"
+            ]
+        },
+        {
+            "English": [
+                "sick"
+            ],
+            "Deutsch": [
+                "krank"
+            ]
+        },
+        {
+            "English": [
+                "background"
+            ],
+            "Deutsch": [
+                "der Hintergrund"
+            ]
+        },
+        {
+            "English": [
+                "eyelash"
+            ],
+            "Deutsch": [
+                "die Wimper"
+            ]
+        },
+        {
+            "English": [
+                "genuine"
+            ],
+            "Deutsch": [
+                "echt, original"
+            ]
+        },
+        {
+            "English": [
+                "parasailing"
+            ],
+            "Deutsch": [
+                "das Parasailing"
+            ]
+        },
+        {
+            "English": [
+                "drawing pin"
+            ],
+            "Deutsch": [
+                "die Rei\u00dfzwecke"
+            ]
+        },
+        {
+            "English": [
+                "drip"
+            ],
+            "Deutsch": [
+                "tropfen"
+            ]
+        },
+        {
+            "English": [
+                "open"
+            ],
+            "Deutsch": [
+                "offen"
+            ]
+        },
+        {
+            "English": [
+                "wax"
+            ],
+            "Deutsch": [
+                "das Wachs"
+            ]
+        },
+        {
+            "English": [
+                "workplace"
+            ],
+            "Deutsch": [
+                "die Arbeitsst\u00e4tte, der Arbeitsplatz"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "English_L5,_6",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
