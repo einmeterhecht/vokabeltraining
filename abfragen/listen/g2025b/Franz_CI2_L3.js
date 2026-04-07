0ead2549bb4a81bc4ff627cda20ca22ef4b5caed
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-04-07 20:18:28 +0200

    add franz

---
+++ abfragen/listen/g2025b/Franz_CI2_L3.js
@@ -0,0 +1,740 @@
+var liste = {
+    "fragen": [
+        {
+            "Französisch": [
+                "möglich"
+            ],
+            "Deutsch": [
+                "possible"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich mit jdm. austauschen"
+            ],
+            "Deutsch": [
+                "échanger avec qn"
+            ]
+        },
+        {
+            "Französisch": [
+                "regelmässig"
+            ],
+            "Deutsch": [
+                "régulier/régulière"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Austausch"
+            ],
+            "Deutsch": [
+                "un échange"
+            ]
+        },
+        {
+            "Französisch": [
+                "eine Postkarte"
+            ],
+            "Deutsch": [
+                "une carte postale"
+            ]
+        },
+        {
+            "Französisch": [
+                "der Sommer"
+            ],
+            "Deutsch": [
+                "l'été (m.)"
+            ]
+        },
+        {
+            "Französisch": [
+                "draussen, im Freien"
+            ],
+            "Deutsch": [
+                "dehors"
+            ]
+        },
+        {
+            "Französisch": [
+                "eine Felsbucht"
+            ],
+            "Deutsch": [
+                "une calanque"
+            ]
+        },
+        {
+            "Französisch": [
+                "der Kajak, das Kajakfahren"
+            ],
+            "Deutsch": [
+                "le kayak"
+            ]
+        },
+        {
+            "Französisch": [
+                "von jdm./etw. Fan sein"
+            ],
+            "Deutsch": [
+                "être fan de qn/qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Abonnement, eine Dauerkarte"
+            ],
+            "Deutsch": [
+                "un abonnement"
+            ]
+        },
+        {
+            "Französisch": [
+                "Das ist der Wahnsinn."
+            ],
+            "Deutsch": [
+                "C'est la folie."
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Korken, hier: ein Verkehrsstau"
+            ],
+            "Deutsch": [
+                "un bouchon"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Gefängnis"
+            ],
+            "Deutsch": [
+                "une prison"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Freund/eine Freundin, ein Kumpel (ugs.)"
+            ],
+            "Deutsch": [
+                "un pote/une pote"
+            ]
+        },
+        {
+            "Französisch": [
+                "eine Gebrauchsanweisung"
+            ],
+            "Deutsch": [
+                "un mode d'emploi"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Marathon"
+            ],
+            "Deutsch": [
+                "un marathon"
+            ]
+        },
+        {
+            "Französisch": [
+                "eine Broschüre"
+            ],
+            "Deutsch": [
+                "une brochure"
+            ]
+        },
+        {
+            "Französisch": [
+                "etw. verpassen, etw. versäumen"
+            ],
+            "Deutsch": [
+                "manquer qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "nachdenken (über), überlegen"
+            ],
+            "Deutsch": [
+                "réfléchir (à)"
+            ]
+        },
+        {
+            "Französisch": [
+                "réfléchir wird konjugiert wie finir."
+            ],
+            "Deutsch": [
+                "je réfléchisterm-45tu réfléchisil/elle/on réfléchitnous réféchissonsvous réfléchissezils/elles réfléchissentp.c.: j'ai réfléchi"
+            ]
+        },
+        {
+            "Französisch": [
+                "Angst haben etw. zu tun"
+            ],
+            "Deutsch": [
+                "avoir peur de faire qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "das Heimweh"
+            ],
+            "Deutsch": [
+                "le mal du pays"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Zweifel"
+            ],
+            "Deutsch": [
+                "un doute"
+            ]
+        },
+        {
+            "Französisch": [
+                "sofort"
+            ],
+            "Deutsch": [
+                "tout de suite"
+            ]
+        },
+        {
+            "Französisch": [
+                "jdn. ermutigen"
+            ],
+            "Deutsch": [
+                "encourager qn"
+            ]
+        },
+        {
+            "Französisch": [
+                "ich ermutige, wir ermutigen"
+            ],
+            "Deutsch": [
+                "j'encourage, nous encourageons"
+            ]
+        },
+        {
+            "Französisch": [
+                "eine Erfahrung"
+            ],
+            "Deutsch": [
+                "une expérience"
+            ]
+        },
+        {
+            "Französisch": [
+                "einzig(artig)"
+            ],
+            "Deutsch": [
+                "unique"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Fortschritt"
+            ],
+            "Deutsch": [
+                "un progrès"
+            ]
+        },
+        {
+            "Französisch": [
+                "eine Kultur"
+            ],
+            "Deutsch": [
+                "une culture"
+            ]
+        },
+        {
+            "Französisch": [
+                "schliesslich, zum Schluss"
+            ],
+            "Deutsch": [
+                "finalement"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich entscheiden"
+            ],
+            "Deutsch": [
+                "se décider"
+            ]
+        },
+        {
+            "Französisch": [
+                "eine Website"
+            ],
+            "Deutsch": [
+                "un site"
+            ]
+        },
+        {
+            "Französisch": [
+                "leicht"
+            ],
+            "Deutsch": [
+                "facile"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich für etw. interessieren"
+            ],
+            "Deutsch": [
+                "s'intéresser à qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "eine Anzeige"
+            ],
+            "Deutsch": [
+                "une annonce"
+            ]
+        },
+        {
+            "Französisch": [
+                "mit jdm. chatten"
+            ],
+            "Deutsch": [
+                "chatter avec qn"
+            ]
+        },
+        {
+            "Französisch": [
+                "etw. behalten"
+            ],
+            "Deutsch": [
+                "garder qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Ordner, eine Akte, hier: Bewerbungsunterlagen"
+            ],
+            "Deutsch": [
+                "un dossier"
+            ]
+        },
+        {
+            "Französisch": [
+                "etw. regeln"
+            ],
+            "Deutsch": [
+                "régler qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "Ostern"
+            ],
+            "Deutsch": [
+                "Pâques (f., pl.)"
+            ]
+        },
+        {
+            "Französisch": [
+                "Kulturwissen"
+            ],
+            "Deutsch": [
+                "En France, ce sont les cloches (Kirchenglocken) et pas les lapins (Hasen) qui apportent les œufs à Pâques."
+            ]
+        },
+        {
+            "Französisch": [
+                "hinunterfahren"
+            ],
+            "Deutsch": [
+                "descendre à (Ortsname)"
+            ]
+        },
+        {
+            "Französisch": [
+                "ungeduldig"
+            ],
+            "Deutsch": [
+                "impatient/impatiente"
+            ]
+        },
+        {
+            "Französisch": [
+                "weggehen"
+            ],
+            "Deutsch": [
+                "s'en aller"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich fühlen"
+            ],
+            "Deutsch": [
+                "se sentir"
+            ]
+        },
+        {
+            "Französisch": [
+                "Konjugation se sentir = partir"
+            ],
+            "Deutsch": [
+                "je me senstu te sensil/elle se sentnous nous sentonsvous vous sentezils/elles se sententp.c.: je me suis senti(e)"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Couscous"
+            ],
+            "Deutsch": [
+                "un couscous"
+            ]
+        },
+        {
+            "Französisch": [
+                "Höhen und Tiefen"
+            ],
+            "Deutsch": [
+                "des hauts et des bas (m. pl.)"
+            ]
+        },
+        {
+            "Französisch": [
+                "aufgeregt, nervös"
+            ],
+            "Deutsch": [
+                "nerveux/nerveuse"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich Mühe geben"
+            ],
+            "Deutsch": [
+                "faire des efforts (m. pl.)"
+            ]
+        },
+        {
+            "Französisch": [
+                "langsam"
+            ],
+            "Deutsch": [
+                "lent/lente"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Akzent (eine Art der Aussprache)"
+            ],
+            "Deutsch": [
+                "un accent"
+            ]
+        },
+        {
+            "Französisch": [
+                "der südfranzösische Akzent"
+            ],
+            "Deutsch": [
+                "l'accent du Midi"
+            ]
+        },
+        {
+            "Französisch": [
+                "am folgenden Tag"
+            ],
+            "Deutsch": [
+                "le lendemain"
+            ]
+        },
+        {
+            "Französisch": [
+                "am Verhungern sein"
+            ],
+            "Deutsch": [
+                "être mort de faim"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich an jdn./etw. gewöhnen"
+            ],
+            "Deutsch": [
+                "s'habituer à qn/qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich Sorgen machen"
+            ],
+            "Deutsch": [
+                "se faire du souci"
+            ]
+        },
+        {
+            "Französisch": [
+                "gut gehen (etw. geht/funktioniert gut)"
+            ],
+            "Deutsch": [
+                "bien se passer"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich über jdn./etw. lustig machen"
+            ],
+            "Deutsch": [
+                "se moquer de qn/qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "durchhalten, dranbleiben"
+            ],
+            "Deutsch": [
+                "s'accrocher"
+            ]
+        },
+        {
+            "Französisch": [
+                "geduldig"
+            ],
+            "Deutsch": [
+                "patient/patiente"
+            ]
+        },
+        {
+            "Französisch": [
+                "jdn./etw. verbessern, jdn. etw. korrigieren"
+            ],
+            "Deutsch": [
+                "corriger qn/qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "ich korrigere, wir korrigieren"
+            ],
+            "Deutsch": [
+                "je corrige, nous corrigeons"
+            ]
+        },
+        {
+            "Französisch": [
+                "besser"
+            ],
+            "Deutsch": [
+                "mieux"
+            ]
+        },
+        {
+            "Französisch": [
+                "vor allem"
+            ],
+            "Deutsch": [
+                "surtout"
+            ]
+        },
+        {
+            "Französisch": [
+                "die französische Umgangssprache"
+            ],
+            "Deutsch": [
+                "le français familier"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Lachanfall"
+            ],
+            "Deutsch": [
+                "un fou rire"
+            ]
+        },
+        {
+            "Französisch": [
+                "etw. garantieren"
+            ],
+            "Deutsch": [
+                "garantir qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "Konjugation garantir wie finir + réfléchir"
+            ],
+            "Deutsch": [
+                "je garantistu garantisil/elle/on garantitnous garantissonsvous garantissezils/elles garantissentp.c. : J'ai garanti"
+            ]
+        },
+        {
+            "Französisch": [
+                "auf jdn./etw. voll abfahren (ugs.)"
+            ],
+            "Deutsch": [
+                "kiffer qn/qc (fam.)"
+            ]
+        },
+        {
+            "Französisch": [
+                "ich lach mich tot (ugs.)"
+            ],
+            "Deutsch": [
+                "mort de rire"
+            ]
+        },
+        {
+            "Französisch": [
+                "Es ist etw los."
+            ],
+            "Deutsch": [
+                "Il y a de l'animation. (f.)"
+            ]
+        },
+        {
+            "Französisch": [
+                "leben"
+            ],
+            "Deutsch": [
+                "vivre"
+            ]
+        },
+        {
+            "Französisch": [
+                "Konjugation von vivre (Stammverkürzung)"
+            ],
+            "Deutsch": [
+                "je vistu visil/elle/on vitnous vivonsvous vivezils/elles viventp.c.: j'ai vécu"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Lebensmittelhändler/eine Lebensmittelhändlerin"
+            ],
+            "Deutsch": [
+                "un épicier/une épicière"
+            ]
+        },
+        {
+            "Französisch": [
+                "über etw. Bescheid wissen"
+            ],
+            "Deutsch": [
+                "être au courant de qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Arzt/eine Ärztin"
+            ],
+            "Deutsch": [
+                "un médecin/une femme médecin"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Krankenhaus/Krankenhäuser"
+            ],
+            "Deutsch": [
+                "un hôpital/des hôpitaux"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Paradies"
+            ],
+            "Deutsch": [
+                "un paradis"
+            ]
+        },
+        {
+            "Französisch": [
+                "ein Eindruck"
+            ],
+            "Deutsch": [
+                "une impression"
+            ]
+        },
+        {
+            "Französisch": [
+                "hier: ein Bericht"
+            ],
+            "Deutsch": [
+                "un rapport"
+            ]
+        },
+        {
+            "Französisch": [
+                "(wieder) weggehen, wegfahren"
+            ],
+            "Deutsch": [
+                "repartir"
+            ]
+        },
+        {
+            "Französisch": [
+                "jdn./etw. wiedersehen"
+            ],
+            "Deutsch": [
+                "revoir qn/qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "jdn. abholen (kommen)"
+            ],
+            "Deutsch": [
+                "venir chercher qn"
+            ]
+        },
+        {
+            "Französisch": [
+                "jdm. für etw. danken, sich bei jdm. für etw. bedanken"
+            ],
+            "Deutsch": [
+                "remercier qn/de pour qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "sich auf etw. vorbereiten"
+            ],
+            "Deutsch": [
+                "se préparer à qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "höflich"
+            ],
+            "Deutsch": [
+                "poli/polie"
+            ]
+        },
+        {
+            "Französisch": [
+                "etw. wiederholen"
+            ],
+            "Deutsch": [
+                "répéter qc"
+            ]
+        },
+        {
+            "Französisch": [
+                "das Gegenteil (von)"
+            ],
+            "Deutsch": [
+                "le contraire (de)"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Franz_CI2_L3",
+    "frage_attribut": "Französisch"
+};
+
+erste_aufgabe();
