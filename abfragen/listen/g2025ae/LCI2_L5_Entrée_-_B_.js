68f8a72eb23361bb3d10c844fd06c1802c712b5d
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-05-03 15:03:27 +0200

    Add franz

---
+++ abfragen/listen/g2025ae/LCI2_L5_Entrée_-_B_.js
@@ -0,0 +1,740 @@
+var liste = {
+    "fragen": [
+        {
+            "Franz\u00f6sisch": [
+                "le Qu\u00e9bec"
+            ],
+            "Deutsch": [
+                "Quebec (Provinz im Osten Kanadas)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un poste"
+            ],
+            "Deutsch": [
+                "eine Stelle/ein Posten"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un ing\u00e9nieur/une ing\u00e9nieure"
+            ],
+            "Deutsch": [
+                "ein Ingenieur/eine Ingenieurin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "accepter qn/qc"
+            ],
+            "Deutsch": [
+                "jdn/etw. akzeptieren/annehmen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un guide touristique"
+            ],
+            "Deutsch": [
+                "ein Reisef\u00fchrer (Buch)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une raison"
+            ],
+            "Deutsch": [
+                "ein Grund"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le printemps"
+            ],
+            "Deutsch": [
+                "der Fr\u00fchling"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une baleine"
+            ],
+            "Deutsch": [
+                "ein Wal"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le bout du monde"
+            ],
+            "Deutsch": [
+                "das Ende der Welt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l'automne (m.)"
+            ],
+            "Deutsch": [
+                "der Herbst"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un canot"
+            ],
+            "Deutsch": [
+                "ein Kanu (kanadisches Franz\u00f6sisch)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l'hiver (m.)"
+            ],
+            "Deutsch": [
+                "der Winter"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le hockey sur glace"
+            ],
+            "Deutsch": [
+                "das Eishockey"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "p\u00eacher qc"
+            ],
+            "Deutsch": [
+                "etw. angeln"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "qu\u00e9b\u00e9cois/qu\u00e9b\u00e9coise"
+            ],
+            "Deutsch": [
+                "aus Quebec"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un paysage"
+            ],
+            "Deutsch": [
+                "eine Landschaft"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une montagne"
+            ],
+            "Deutsch": [
+                "ein Gebirge"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un fleuve"
+            ],
+            "Deutsch": [
+                "ein Fluss, ein Strom"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un lac"
+            ],
+            "Deutsch": [
+                "ein See"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une for\u00eat"
+            ],
+            "Deutsch": [
+                "ein Wald"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un gratte-ciel"
+            ],
+            "Deutsch": [
+                "ein Wolkenkratzer"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "typique de qn"
+            ],
+            "Deutsch": [
+                "typisch f\u00fcr jdn./etw."
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "joyeux/joyeuse"
+            ],
+            "Deutsch": [
+                "fr\u00f6hlich"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une province"
+            ],
+            "Deutsch": [
+                "eine Provinz"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une conf\u00e9rence"
+            ],
+            "Deutsch": [
+                "ein Vortrag/eine Konferenz"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un peuple"
+            ],
+            "Deutsch": [
+                "ein Volk"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un peuple autochtone"
+            ],
+            "Deutsch": [
+                "Ureinwohner"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un/une Inuit"
+            ],
+            "Deutsch": [
+                "ein/eine Inuit (indigene Volksgruppe in Kanada)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "commencer par qc"
+            ],
+            "Deutsch": [
+                "mit etw. beginnen, mit etw. anfangen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la s\u00e9curit\u00e9"
+            ],
+            "Deutsch": [
+                "die Sicherheit"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un changement climatique"
+            ],
+            "Deutsch": [
+                "ein Klimawandel"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00eatre fascin\u00e9/fascin\u00e9e (par)"
+            ],
+            "Deutsch": [
+                "fasziniert sein (von)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "appartenir \u00e0 qc/qn"
+            ],
+            "Deutsch": [
+                "zu etw./jdm. geh\u00f6ren, etw. angeh\u00f6ren"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une tribu"
+            ],
+            "Deutsch": [
+                "ein (Volks)Stamm"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un secret"
+            ],
+            "Deutsch": [
+                "ein Geheimnis"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un tipi"
+            ],
+            "Deutsch": [
+                "ein Tipi"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un r\u00eave"
+            ],
+            "Deutsch": [
+                "ein Traum"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "faire une surprise \u00e0 qn"
+            ],
+            "Deutsch": [
+                "jdn. \u00fcberraschen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une promenade"
+            ],
+            "Deutsch": [
+                "ein Spaziergang, ein Ausflug"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "inviter qn \u00e0 faire qc"
+            ],
+            "Deutsch": [
+                "jdn einladen, etw. zu tun"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une relation"
+            ],
+            "Deutsch": [
+                "eine Beziehung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "fort/forte"
+            ],
+            "Deutsch": [
+                "stark"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la nature"
+            ],
+            "Deutsch": [
+                "die Natur"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "go\u00fbter qc"
+            ],
+            "Deutsch": [
+                "etw. probieren"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une omelette"
+            ],
+            "Deutsch": [
+                "ein Omelett"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le sirop d'\u00e9rable"
+            ],
+            "Deutsch": [
+                "der Ahornsirup"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une soupe"
+            ],
+            "Deutsch": [
+                "eine Suppe"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une pomme de terre"
+            ],
+            "Deutsch": [
+                "eine Kartoffel"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un moustique"
+            ],
+            "Deutsch": [
+                "eine M\u00fccke"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une mission"
+            ],
+            "Deutsch": [
+                "eine Mission, eine Aufgabe"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "avoir pour mission de faire qc"
+            ],
+            "Deutsch": [
+                "den Auftrag haben, etw. zu tun"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une fa\u00e7on"
+            ],
+            "Deutsch": [
+                "eine Art, eine Weise"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "d'ailleurs"
+            ],
+            "Deutsch": [
+                "\u00fcbrigens"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "agir"
+            ],
+            "Deutsch": [
+                "handeln"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "prot\u00e9ger qn/qc"
+            ],
+            "Deutsch": [
+                "jdn./etw. sch\u00fctzen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l'environnement (m.)"
+            ],
+            "Deutsch": [
+                "die Umwelt, die Umgebung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "chacun/chacune"
+            ],
+            "Deutsch": [
+                "jeder/jede/jedes"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l'Am\u00e9rique (f.)"
+            ],
+            "Deutsch": [
+                "Amerika"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une version"
+            ],
+            "Deutsch": [
+                "eine Fassung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un mari"
+            ],
+            "Deutsch": [
+                "ein Ehemann"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un/une coll\u00e8gue"
+            ],
+            "Deutsch": [
+                "ein Kollege/eine Kollegin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l\u00e0-bas"
+            ],
+            "Deutsch": [
+                "dort(hin), da(hin)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une aide"
+            ],
+            "Deutsch": [
+                "eine Hilfe, eine Unterst\u00fctzung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un emploi"
+            ],
+            "Deutsch": [
+                "eine Anstellung, eine Arbeit"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "continuer qc"
+            ],
+            "Deutsch": [
+                "mit etw. weitermachen, mit etw. fortfahren"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un a\u00e9roport"
+            ],
+            "Deutsch": [
+                "ein Flughafen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "avoir l'impression de faire qc"
+            ],
+            "Deutsch": [
+                "den Eindruck haben etw. zu tun"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00e9tonn\u00e9/\u00e9tonn\u00e9e"
+            ],
+            "Deutsch": [
+                "erstaunt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un Qu\u00e9b\u00e9cois/une Qu\u00e9b\u00e9coise"
+            ],
+            "Deutsch": [
+                "ein Quebecer/eine Quebecerin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une expression"
+            ],
+            "Deutsch": [
+                "ein Ausdruck"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "bon march\u00e9"
+            ],
+            "Deutsch": [
+                "g\u00fcnstig"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "craindre qn/qc"
+            ],
+            "Deutsch": [
+                "jdn./etw. f\u00fcrchten"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "d'occasion"
+            ],
+            "Deutsch": [
+                "Gebraucht-"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "se plaindre de qn/qc"
+            ],
+            "Deutsch": [
+                "sich \u00fcber jdn/etw beschweren"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "avoir mal (\u00e0 qc)"
+            ],
+            "Deutsch": [
+                "Schmerzen haben"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le ventre"
+            ],
+            "Deutsch": [
+                "der Bauch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une appendicite"
+            ],
+            "Deutsch": [
+                "eine Blinddarmentz\u00fcndung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une urgence"
+            ],
+            "Deutsch": [
+                "ein Notfall"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la fi\u00e8vre"
+            ],
+            "Deutsch": [
+                "das Fieber"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "autrement"
+            ],
+            "Deutsch": [
+                "anders"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un patient/une patiente"
+            ],
+            "Deutsch": [
+                "ein Patient/eine Patientin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un rendez-vous"
+            ],
+            "Deutsch": [
+                "ein Termin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "prendre rendez-vous (chez/avec qn)"
+            ],
+            "Deutsch": [
+                "einen Termin ausmachen (bei/mit jdm)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un docteur"
+            ],
+            "Deutsch": [
+                "ein Doktor, ein Arzt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la t\u00eate"
+            ],
+            "Deutsch": [
+                "der Kopf"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la gorge"
+            ],
+            "Deutsch": [
+                "der Hals"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une consultation"
+            ],
+            "Deutsch": [
+                "eine Sprechstunde, ein Arztbesuch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un m\u00e9dicament"
+            ],
+            "Deutsch": [
+                "ein Arzneimittel, ein Medikament"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une ordonnance"
+            ],
+            "Deutsch": [
+                "ein Rezept (vom Arzt)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "faire une ordonnance \u00e0 qn"
+            ],
+            "Deutsch": [
+                "jdm. ein Rezept ausstellen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une excursion"
+            ],
+            "Deutsch": [
+                "ein Ausflug"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "LCI2_L5_Entr\u00e9e_-_B_",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
