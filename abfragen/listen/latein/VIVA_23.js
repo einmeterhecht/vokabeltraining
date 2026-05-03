e33583faf334a9f00ccf390ede9ff2eacb4b8e83
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-05-03 09:44:04 +0200

    add latein

---
+++ abfragen/listen/latein/VIVA_23.js
@@ -0,0 +1,204 @@
+var liste = {
+    "fragen": [
+        {
+            "Latein": [
+                "opt\u0101re 1"
+            ],
+            "Deutsch": [
+                "w\u00fcnschen"
+            ]
+        },
+        {
+            "Latein": [
+                "alter, altera, alterum"
+            ],
+            "Deutsch": [
+                "der andere, der zweite"
+            ]
+        },
+        {
+            "Latein": [
+                "v\u012bnum, -\u012b n."
+            ],
+            "Deutsch": [
+                "Wein"
+            ]
+        },
+        {
+            "Latein": [
+                "imm\u014d"
+            ],
+            "Deutsch": [
+                "nein vielmehr, ja sogar"
+            ]
+        },
+        {
+            "Latein": [
+                "m\u014ds, m\u014dris m."
+            ],
+            "Deutsch": [
+                "Sitte, BrauchPl. auch: Charakter"
+            ]
+        },
+        {
+            "Latein": [
+                "enim (nachgestellt)"
+            ],
+            "Deutsch": [
+                "n\u00e4mlich, denn"
+            ]
+        },
+        {
+            "Latein": [
+                "cre\u0101re 1"
+            ],
+            "Deutsch": [
+                "erschaffen, w\u00e4hlen"
+            ]
+        },
+        {
+            "Latein": [
+                "igitur (nachgestellt)"
+            ],
+            "Deutsch": [
+                "also, folglich"
+            ]
+        },
+        {
+            "Latein": [
+                "bibere, bib\u014d, bib\u012b, - 3k"
+            ],
+            "Deutsch": [
+                "trinken"
+            ]
+        },
+        {
+            "Latein": [
+                "s\u0113d\u0113s, s\u0113dis f."
+            ],
+            "Deutsch": [
+                "1. Sitz2. Wohnsitz3. Heimat"
+            ]
+        },
+        {
+            "Latein": [
+                "obtin\u0113re, obtine\u014d, obtinu\u012b, obtentum 2"
+            ],
+            "Deutsch": [
+                "innehaben, (besetzt) halten"
+            ]
+        },
+        {
+            "Latein": [
+                "sol\u0113re 2"
+            ],
+            "Deutsch": [
+                "gew\u00f6hnlich tun, gewohnt sein"
+            ]
+        },
+        {
+            "Latein": [
+                "temper\u0101re 1"
+            ],
+            "Deutsch": [
+                "Mass halten,"
+            ]
+        },
+        {
+            "Latein": [
+                "n\u0113 (+ Konj.)"
+            ],
+            "Deutsch": [
+                "dass nicht, damit nicht"
+            ]
+        },
+        {
+            "Latein": [
+                "tim\u0113re/metuere n\u0113"
+            ],
+            "Deutsch": [
+                "f\u00fcrchten, dass"
+            ]
+        },
+        {
+            "Latein": [
+                "t\u0101lis, e"
+            ],
+            "Deutsch": [
+                "solch ein, derartig"
+            ]
+        },
+        {
+            "Latein": [
+                "decet (+ Inf./+ AcI)"
+            ],
+            "Deutsch": [
+                "es geh\u00f6rt sich"
+            ]
+        },
+        {
+            "Latein": [
+                "serm\u014d, serm\u014dnis m."
+            ],
+            "Deutsch": [
+                "1. Gespr\u00e4ch2. Redeweise3. Sprache"
+            ]
+        },
+        {
+            "Latein": [
+                "vitium, -\u012b n."
+            ],
+            "Deutsch": [
+                "Fehler, schlechte Eigenschaft"
+            ]
+        },
+        {
+            "Latein": [
+                "qu\u0101 r\u0113/qu\u0101r\u0113"
+            ],
+            "Deutsch": [
+                "weshalb, deshalb"
+            ]
+        },
+        {
+            "Latein": [
+                "l\u012bbert\u0101s, l\u012bbert\u0101tis f."
+            ],
+            "Deutsch": [
+                "Freiheit"
+            ]
+        },
+        {
+            "Latein": [
+                "accidere, accid\u014d, accid\u012b, - 3k"
+            ],
+            "Deutsch": [
+                "sich ereignen, geschehen"
+            ]
+        },
+        {
+            "Latein": [
+                "accidit, ut"
+            ],
+            "Deutsch": [
+                "es ereignet sich, dass"
+            ]
+        },
+        {
+            "Latein": [
+                "clam (Adv.)"
+            ],
+            "Deutsch": [
+                "heimlich"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "VIVA_23",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
