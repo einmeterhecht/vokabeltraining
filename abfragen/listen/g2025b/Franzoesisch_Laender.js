127fcb9e5b165e2747004163128afb90f38ce483
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-05-10 08:43:48 +0200

    add Franz

---
+++ abfragen/listen/g2025b/Franzoesisch_Laender.js
@@ -0,0 +1,180 @@
+var liste = {
+    "fragen": [
+        {
+            "Franz\u00f6sisch": [
+                "la France"
+            ],
+            "Deutsch": [
+                "Frankreich"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "L'Espagne"
+            ],
+            "Deutsch": [
+                "Spanien"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "La Belgique"
+            ],
+            "Deutsch": [
+                "Belgien"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "L'Italie"
+            ],
+            "Deutsch": [
+                "Italien"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Le Portugal"
+            ],
+            "Deutsch": [
+                "Portugal"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Le Maroc"
+            ],
+            "Deutsch": [
+                "Marokko"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Le Canada"
+            ],
+            "Deutsch": [
+                "Kanada"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Les Pays-Bas"
+            ],
+            "Deutsch": [
+                "Niederlande"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Les Etats-Unis"
+            ],
+            "Deutsch": [
+                "Vereinigten Staten"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Les Phillipines"
+            ],
+            "Deutsch": [
+                "Philipinen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Le Mexique"
+            ],
+            "Deutsch": [
+                "Mexiko"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Le Mozambique"
+            ],
+            "Deutsch": [
+                "Mosambik"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le Zimbabwe"
+            ],
+            "Deutsch": [
+                "Simbabwe"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "L'Angleterre"
+            ],
+            "Deutsch": [
+                "England"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "La Tuquie"
+            ],
+            "Deutsch": [
+                "T\u00fcrkei"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "L'Iran"
+            ],
+            "Deutsch": [
+                "Iran"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "L'Irak"
+            ],
+            "Deutsch": [
+                "Irak"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "L'Israel"
+            ],
+            "Deutsch": [
+                "Israel"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Le Br\u00e9sil"
+            ],
+            "Deutsch": [
+                "Brasilien"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Le P\u00e9rou"
+            ],
+            "Deutsch": [
+                "Peru"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "Le Venezuela"
+            ],
+            "Deutsch": [
+                "Venezuela"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Franzoesisch_Laender",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
