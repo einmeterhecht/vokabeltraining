8165a489ee2c801b61a8874e7dc8af5b2b58da3c
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-05-10 08:27:20 +0200

    addfrench

---
+++ abfragen/listen/g2025b/Fransösisch_L4_entrée.js
@@ -0,0 +1,212 @@
+var liste = {
+    "fragen": [
+        {
+            "Franz\u00f6sisch": [
+                "une langue"
+            ],
+            "Deutsch": [
+                "eine Sprache"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la Belgique"
+            ],
+            "Deutsch": [
+                "Belgien"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le coeur"
+            ],
+            "Deutsch": [
+                "das Herz"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "au coeur de"
+            ],
+            "Deutsch": [
+                "im Herzen von"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le Luxembourg"
+            ],
+            "Deutsch": [
+                "Luxemburg"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "les Pays-Bas (m., pl.)"
+            ],
+            "Deutsch": [
+                "die Niederlande"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la Wallonie"
+            ],
+            "Deutsch": [
+                "Wallonien (Region in Belgien)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la Flandre"
+            ],
+            "Deutsch": [
+                "Flandern (Region in Belgien)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le flamand"
+            ],
+            "Deutsch": [
+                "Fl\u00e4misch (in Belgien gesprochenes Niederl\u00e4ndisch)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un million"
+            ],
+            "Deutsch": [
+                "eine Million"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un habitant, une habitante"
+            ],
+            "Deutsch": [
+                "ein Einwohner/eine Einwohnerin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une langue officielle"
+            ],
+            "Deutsch": [
+                "eine Amtssprache"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le n\u00e9erlandais"
+            ],
+            "Deutsch": [
+                "Niederl\u00e4ndisch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une destination"
+            ],
+            "Deutsch": [
+                "ein Ziel, ein Bestimmungsort"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la d\u00e9tente"
+            ],
+            "Deutsch": [
+                "die Entspannung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "la mer du Nord"
+            ],
+            "Deutsch": [
+                "die Nordsee"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un symbole"
+            ],
+            "Deutsch": [
+                "ein Symbol"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une capitale"
+            ],
+            "Deutsch": [
+                "eine Hauptstadt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "cosmopolite"
+            ],
+            "Deutsch": [
+                "kosmopolitisch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "bilingue"
+            ],
+            "Deutsch": [
+                "zweisprachig"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un Belge/une Belge"
+            ],
+            "Deutsch": [
+                "ein Belgier/eine Belgierin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "des frites (f., pl.)"
+            ],
+            "Deutsch": [
+                "Pommes frites"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une gaufre"
+            ],
+            "Deutsch": [
+                "eine Waffel"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "belge"
+            ],
+            "Deutsch": [
+                "belgisch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un dessinateur/une dessinatrice"
+            ],
+            "Deutsch": [
+                "ein Zeichner/eine Zeichnerin"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Frans\u00f6sisch_L4_entr\u00e9e",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
