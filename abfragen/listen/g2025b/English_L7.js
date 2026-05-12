9e6538738c77479d9a4f92193acb7f575d1f8242
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-05-12 14:46:09 +0200

    add enlish

---
+++ abfragen/listen/g2025b/English_L7.js
@@ -0,0 +1,620 @@
+var liste = {
+    "fragen": [
+        {
+            "English": [
+                "cramped"
+            ],
+            "Deutsch": [
+                "überfüllt"
+            ]
+        },
+        {
+            "English": [
+                "flatmate"
+            ],
+            "Deutsch": [
+                "der/die Mitbewohner/-in"
+            ]
+        },
+        {
+            "English": [
+                "habitat"
+            ],
+            "Deutsch": [
+                "der Lebensraum"
+            ]
+        },
+        {
+            "English": [
+                "privacy"
+            ],
+            "Deutsch": [
+                "der Datenschutz"
+            ]
+        },
+        {
+            "English": [
+                "shared"
+            ],
+            "Deutsch": [
+                "gemeinsam genutzt"
+            ]
+        },
+        {
+            "English": [
+                "shelter"
+            ],
+            "Deutsch": [
+                "der Unterstand/ Schutz"
+            ]
+        },
+        {
+            "English": [
+                "atmosphere"
+            ],
+            "Deutsch": [
+                "die Atmosphäre/ Stimmung"
+            ]
+        },
+        {
+            "English": [
+                "bear"
+            ],
+            "Deutsch": [
+                "der Bär"
+            ]
+        },
+        {
+            "English": [
+                "beaver"
+            ],
+            "Deutsch": [
+                "der Biber"
+            ]
+        },
+        {
+            "English": [
+                "block"
+            ],
+            "Deutsch": [
+                "der Häuserblock"
+            ]
+        },
+        {
+            "English": [
+                "built-up"
+            ],
+            "Deutsch": [
+                "bebaut"
+            ]
+        },
+        {
+            "English": [
+                "financial"
+            ],
+            "Deutsch": [
+                "finanziell, Finanz-"
+            ]
+        },
+        {
+            "English": [
+                "grassland"
+            ],
+            "Deutsch": [
+                "die Grasfläche"
+            ]
+        },
+        {
+            "English": [
+                "marsh"
+            ],
+            "Deutsch": [
+                "die Marsch, der Sumpf"
+            ]
+        },
+        {
+            "English": [
+                "neighbourhood"
+            ],
+            "Deutsch": [
+                "die Nachbar-schaft/ Wohn-gegend"
+            ]
+        },
+        {
+            "English": [
+                "potential"
+            ],
+            "Deutsch": [
+                "das Potenzial"
+            ]
+        },
+        {
+            "English": [
+                "resident"
+            ],
+            "Deutsch": [
+                "der/die Anwohner/-in"
+            ]
+        },
+        {
+            "English": [
+                "skin"
+            ],
+            "Deutsch": [
+                "das Fell"
+            ]
+        },
+        {
+            "English": [
+                "skyscraper"
+            ],
+            "Deutsch": [
+                "der Wolkenkratzer"
+            ]
+        },
+        {
+            "English": [
+                "spot"
+            ],
+            "Deutsch": [
+                "die Stelle"
+            ]
+        },
+        {
+            "English": [
+                "turkey"
+            ],
+            "Deutsch": [
+                "der Truthahn"
+            ]
+        },
+        {
+            "English": [
+                "brick"
+            ],
+            "Deutsch": [
+                "der Backstein"
+            ]
+        },
+        {
+            "English": [
+                "cloth"
+            ],
+            "Deutsch": [
+                "der Stoff"
+            ]
+        },
+        {
+            "English": [
+                "design"
+            ],
+            "Deutsch": [
+                "das Design, die Gestaltung"
+            ]
+        },
+        {
+            "English": [
+                "flood"
+            ],
+            "Deutsch": [
+                "die Überschwem-mung"
+            ]
+        },
+        {
+            "English": [
+                "ger"
+            ],
+            "Deutsch": [
+                "die Jurte"
+            ]
+        },
+        {
+            "English": [
+                "rock"
+            ],
+            "Deutsch": [
+                "der Stein"
+            ]
+        },
+        {
+            "English": [
+                "stilt"
+            ],
+            "Deutsch": [
+                "die Stelze, der Pfahl"
+            ]
+        },
+        {
+            "English": [
+                "untidy"
+            ],
+            "Deutsch": [
+                "unordentlich"
+            ]
+        },
+        {
+            "English": [
+                "artist"
+            ],
+            "Deutsch": [
+                "der/die Künstler/-in"
+            ]
+        },
+        {
+            "English": [
+                "bench"
+            ],
+            "Deutsch": [
+                "die Sitzbank"
+            ]
+        },
+        {
+            "English": [
+                "benefit"
+            ],
+            "Deutsch": [
+                "der Nutzen/Vorteil"
+            ]
+        },
+        {
+            "English": [
+                "centre"
+            ],
+            "Deutsch": [
+                "das Zentrum, die Mitte"
+            ]
+        },
+        {
+            "English": [
+                "clap"
+            ],
+            "Deutsch": [
+                "klatschen"
+            ]
+        },
+        {
+            "English": [
+                "coast"
+            ],
+            "Deutsch": [
+                "die Küste"
+            ]
+        },
+        {
+            "English": [
+                "development"
+            ],
+            "Deutsch": [
+                "die Bauplanung, das Bauprojekt"
+            ]
+        },
+        {
+            "English": [
+                "dreamy"
+            ],
+            "Deutsch": [
+                "verträumt"
+            ]
+        },
+        {
+            "English": [
+                "economy"
+            ],
+            "Deutsch": [
+                "die Wirtschaft"
+            ]
+        },
+        {
+            "English": [
+                "energy"
+            ],
+            "Deutsch": [
+                "die Energie"
+            ]
+        },
+        {
+            "English": [
+                "fact"
+            ],
+            "Deutsch": [
+                "die Tatsache"
+            ]
+        },
+        {
+            "English": [
+                "history"
+            ],
+            "Deutsch": [
+                "die Geschichte"
+            ]
+        },
+        {
+            "English": [
+                "improvise"
+            ],
+            "Deutsch": [
+                "improvisieren"
+            ]
+        },
+        {
+            "English": [
+                "nation"
+            ],
+            "Deutsch": [
+                "der Staat"
+            ]
+        },
+        {
+            "English": [
+                "nature"
+            ],
+            "Deutsch": [
+                "die Natur"
+            ]
+        },
+        {
+            "English": [
+                "nod"
+            ],
+            "Deutsch": [
+                "nicken"
+            ]
+        },
+        {
+            "English": [
+                "origin"
+            ],
+            "Deutsch": [
+                "der Ursprung, die Herkunft"
+            ]
+        },
+        {
+            "English": [
+                "parade"
+            ],
+            "Deutsch": [
+                "die Parade, der Umzug"
+            ]
+        },
+        {
+            "English": [
+                "person"
+            ],
+            "Deutsch": [
+                "die Person"
+            ]
+        },
+        {
+            "English": [
+                "reforestation"
+            ],
+            "Deutsch": [
+                "die Wiederauf-forstung"
+            ]
+        },
+        {
+            "English": [
+                "respectfully"
+            ],
+            "Deutsch": [
+                "respektvoll"
+            ]
+        },
+        {
+            "English": [
+                "rhyme"
+            ],
+            "Deutsch": [
+                "der Reim"
+            ]
+        },
+        {
+            "English": [
+                "romance"
+            ],
+            "Deutsch": [
+                "die Liebesgeschich-te, Anfang einer Beziehung"
+            ]
+        },
+        {
+            "English": [
+                "shine"
+            ],
+            "Deutsch": [
+                "glänzen, leuchten"
+            ]
+        },
+        {
+            "English": [
+                "tap"
+            ],
+            "Deutsch": [
+                "schlagen, klopfen"
+            ]
+        },
+        {
+            "English": [
+                "working-class"
+            ],
+            "Deutsch": [
+                "die Arbeiterklasse"
+            ]
+        },
+        {
+            "English": [
+                "budget"
+            ],
+            "Deutsch": [
+                "das Budget, der Etat"
+            ]
+        },
+        {
+            "English": [
+                "fit"
+            ],
+            "Deutsch": [
+                "fit, in Form"
+            ]
+        },
+        {
+            "English": [
+                "mix"
+            ],
+            "Deutsch": [
+                "die Mischung"
+            ]
+        },
+        {
+            "English": [
+                "nightmare"
+            ],
+            "Deutsch": [
+                "der Albtraum"
+            ]
+        },
+        {
+            "English": [
+                "picturesque"
+            ],
+            "Deutsch": [
+                "pittoresk"
+            ]
+        },
+        {
+            "English": [
+                "reasonable"
+            ],
+            "Deutsch": [
+                "angemessen, vernünftig"
+            ]
+        },
+        {
+            "English": [
+                "stage"
+            ],
+            "Deutsch": [
+                "die Stufe/Phase"
+            ]
+        },
+        {
+            "English": [
+                "atmosphere"
+            ],
+            "Deutsch": [
+                "die Luft-/Gashülle"
+            ]
+        },
+        {
+            "English": [
+                "bright"
+            ],
+            "Deutsch": [
+                "hell"
+            ]
+        },
+        {
+            "English": [
+                "cordless"
+            ],
+            "Deutsch": [
+                "kabellos"
+            ]
+        },
+        {
+            "English": [
+                "downtown"
+            ],
+            "Deutsch": [
+                "die Innenstadt, das Stadtgebiet"
+            ]
+        },
+        {
+            "English": [
+                "gasoline"
+            ],
+            "Deutsch": [
+                "der Kraftstoff"
+            ]
+        },
+        {
+            "English": [
+                "ironic"
+            ],
+            "Deutsch": [
+                "ironisch"
+            ]
+        },
+        {
+            "English": [
+                "Milky Way"
+            ],
+            "Deutsch": [
+                "die Milchstraße"
+            ]
+        },
+        {
+            "English": [
+                "noise"
+            ],
+            "Deutsch": [
+                "das Störgeräusch, das Rauschen"
+            ]
+        },
+        {
+            "English": [
+                "signal"
+            ],
+            "Deutsch": [
+                "das Signal"
+            ]
+        },
+        {
+            "English": [
+                "store"
+            ],
+            "Deutsch": [
+                "das Geschäft, der Laden"
+            ]
+        },
+        {
+            "English": [
+                "telescope"
+            ],
+            "Deutsch": [
+                "das Teleskop"
+            ]
+        },
+        {
+            "English": [
+                "unique"
+            ],
+            "Deutsch": [
+                "einzigartig"
+            ]
+        },
+        {
+            "English": [
+                "utilize"
+            ],
+            "Deutsch": [
+                "verwenden"
+            ]
+        },
+        {
+            "English": [
+                "wireless"
+            ],
+            "Deutsch": [
+                "drahtlos"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "English_L7",
+    "frage_attribut": "English"
+};
+
+erste_aufgabe();
