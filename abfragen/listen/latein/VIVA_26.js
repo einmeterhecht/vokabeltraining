64126a55ecd41fd86f5e430ac54cdf62f2a9147e
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-08-25 21:04:49 +0200

    as

---
+++ abfragen/listen/latein/VIVA_26.js
@@ -0,0 +1,196 @@
+var liste = {
+    "fragen": [
+        {
+            "Franz\u00f6sisch": [
+                "quamv\u012bs (+ Konjunktiv)"
+            ],
+            "Deutsch": [
+                "obwohl, wenn auch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "excit\u0101re 1"
+            ],
+            "Deutsch": [
+                "antreiben, ermuntern, wecken"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ruere 3k, ru\u014d, ru\u012b, rutum"
+            ],
+            "Deutsch": [
+                "1. eilen, st\u00fcrmen2. einst\u00fcrzen, herabst\u00fcrzen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "for\u0101s (Adv.)"
+            ],
+            "Deutsch": [
+                "heraus, hinaus"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "d\u0113mum (Adv.)"
+            ],
+            "Deutsch": [
+                "endlich"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "statuere 3k, statu\u014d, statu\u012b, stat\u016btum"
+            ],
+            "Deutsch": [
+                "1. aufstellen2. festsetzen, beschliessen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "s\u012bcut (Adv.)"
+            ],
+            "Deutsch": [
+                "so wie"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "interim (Adv.)"
+            ],
+            "Deutsch": [
+                "inzwischen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "ub\u012bque (Adv.)"
+            ],
+            "Deutsch": [
+                "\u00fcberall"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "vetus, veteris (Abl. vetere)"
+            ],
+            "Deutsch": [
+                "alt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "parum (Adv.)"
+            ],
+            "Deutsch": [
+                "(zu) wenig"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "celer, celeris, celere"
+            ],
+            "Deutsch": [
+                "schnell"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "adh\u016bc (Adv.)"
+            ],
+            "Deutsch": [
+                "noch"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "iuvenis, iuvenis m./f."
+            ],
+            "Deutsch": [
+                "jung, junger Mann, junge Frau"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "tendere 3k, tend\u014d, tetend\u012b, tentum"
+            ],
+            "Deutsch": [
+                "1. spannen, ausstrecken2. streben"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "via, -ae f."
+            ],
+            "Deutsch": [
+                "Weg, Stra\u00dfe"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "c\u0113dere 3k, c\u0113d\u014d, cess\u012b, cessum"
+            ],
+            "Deutsch": [
+                "gehen, weichen, nachgeben"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "undique (Adv.)"
+            ],
+            "Deutsch": [
+                "von allen Seiten"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "d\u0113p\u014dnere 3k, d\u0113p\u014dn\u014d, d\u0113posu\u012b, d\u0113positum"
+            ],
+            "Deutsch": [
+                "1. ablegen2. aufgeben"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "tenebrae, -\u0101rum f.Pl."
+            ],
+            "Deutsch": [
+                "Dunkelheita"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "metus, -\u016bs m."
+            ],
+            "Deutsch": [
+                "Furcht, Besorgnis"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "val\u0113re 2, vale\u014d, valu\u012b, -"
+            ],
+            "Deutsch": [
+                "1. gesund sein2. stark sein3. imstande sein"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "val\u0113! val\u0113te!"
+            ],
+            "Deutsch": [
+                "lebe wohl!/lebt wohl!"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "VIVA_26",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
