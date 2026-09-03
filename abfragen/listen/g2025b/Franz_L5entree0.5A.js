4a730645fd7382815f4c0cdd6a9947b771cf1d87
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-09-03 10:25:47 +0200

    add

---
+++ abfragen/listen/g2025b/Franz_L5entree0.5A.js
@@ -0,0 +1,332 @@
+var liste = {
+    "fragen": [
+        {
+            "Franz\u00f6sisch": [
+                "le Qu\u00e9bec"
+            ],
+            "Deutsch": [
+                "Quebec (Provinz im Osten Kanadas)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un poste"
+            ],
+            "Deutsch": [
+                "eine Stelle; ein Posten"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un ing\u00e9nieur"
+            ],
+            "Deutsch": [
+                "ein Ingenieur"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une ing\u00e9nieure"
+            ],
+            "Deutsch": [
+                "eine Ingenieurin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "accepter qn/qc"
+            ],
+            "Deutsch": [
+                "jdn./etw. akzeptieren; annehmen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un guide touristique"
+            ],
+            "Deutsch": [
+                "ein Reisef\u00fchrer (Buch)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une raison"
+            ],
+            "Deutsch": [
+                "ein Grund"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le printemps"
+            ],
+            "Deutsch": [
+                "der Fr\u00fchling"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une baleine"
+            ],
+            "Deutsch": [
+                "ein Wal"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le bout du monde"
+            ],
+            "Deutsch": [
+                "das Ende der Welt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l\u2019automne"
+            ],
+            "Deutsch": [
+                "der Herbst"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un canot"
+            ],
+            "Deutsch": [
+                "ein Kanu"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l\u2019hiver"
+            ],
+            "Deutsch": [
+                "der Winter"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le hockey sur glace"
+            ],
+            "Deutsch": [
+                "das Eishockey"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "le carnaval"
+            ],
+            "Deutsch": [
+                "der Karneval"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "p\u00eacher qc"
+            ],
+            "Deutsch": [
+                "etw. angeln"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "qu\u00e9b\u00e9cois/qu\u00e9b\u00e9coise"
+            ],
+            "Deutsch": [
+                "aus Quebec"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un paysage"
+            ],
+            "Deutsch": [
+                "eine Landschaft"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une montagne"
+            ],
+            "Deutsch": [
+                "ein Gebirge"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un fleuve"
+            ],
+            "Deutsch": [
+                "ein Fluss; ein Strom"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un lac"
+            ],
+            "Deutsch": [
+                "ein See"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une for\u00eat"
+            ],
+            "Deutsch": [
+                "ein Wald"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un gratte-ciel"
+            ],
+            "Deutsch": [
+                "ein Wolkenkratzer"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "typique de qn/qc"
+            ],
+            "Deutsch": [
+                "typisch f\u00fcr jdn./etw."
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "joyeux/joyeuse"
+            ],
+            "Deutsch": [
+                "fr\u00f6hlich"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une province"
+            ],
+            "Deutsch": [
+                "eine Provinz"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l\u2019Am\u00e9rique (f.)"
+            ],
+            "Deutsch": [
+                "Amerika"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une version"
+            ],
+            "Deutsch": [
+                "eine Fassung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un mari"
+            ],
+            "Deutsch": [
+                "ein Ehemann"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un/une coll\u00e8gue"
+            ],
+            "Deutsch": [
+                "ein Kollege/eine Kollegin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une aide"
+            ],
+            "Deutsch": [
+                "eine Hilfe; eine Unterst\u00fctzung"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un emploi"
+            ],
+            "Deutsch": [
+                "eine Anstellung; eine Arbeit"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "l\u00e0-bas"
+            ],
+            "Deutsch": [
+                "dort(hin); da(hin)"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "continuer qc"
+            ],
+            "Deutsch": [
+                "mit etw. weitermachen; mit etw. fortfahren"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un a\u00e9roport"
+            ],
+            "Deutsch": [
+                "ein Flughafen"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "\u00e9tonn\u00e9/\u00e9tonn\u00e9e"
+            ],
+            "Deutsch": [
+                "erstaunt"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "avoir l\u2019impression de faire qc"
+            ],
+            "Deutsch": [
+                "den Eindruck haben, etw. zu tun"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "un Qu\u00e9b\u00e9cois/une Qu\u00e9b\u00e9coise"
+            ],
+            "Deutsch": [
+                "ein Quebecer/eine Quebecerin"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "une expression"
+            ],
+            "Deutsch": [
+                "ein Ausdruck"
+            ]
+        },
+        {
+            "Franz\u00f6sisch": [
+                "bon march\u00e9"
+            ],
+            "Deutsch": [
+                "g\u00fcnstig"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Franz_L5entree0.5A",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
