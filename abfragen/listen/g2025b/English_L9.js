15649f2a1eb23d22b7a0be2b7bf1e33dfab46b89
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-09-05 11:28:50 +0200

    add

---
+++ abfragen/listen/g2025b/English_L9.js
@@ -0,0 +1,356 @@
+var liste = {
+    "fragen": [
+        {
+            "English": [
+                "cosmetics"
+            ],
+            "Deutsch": [
+                "die Kosmetika"
+            ]
+        },
+        {
+            "English": [
+                "toiletries"
+            ],
+            "Deutsch": [
+                "die K\u00f6rperpflege-artikel"
+            ]
+        },
+        {
+            "English": [
+                "amount"
+            ],
+            "Deutsch": [
+                "die Menge"
+            ]
+        },
+        {
+            "English": [
+                "distribute"
+            ],
+            "Deutsch": [
+                "verteilen"
+            ]
+        },
+        {
+            "English": [
+                "package"
+            ],
+            "Deutsch": [
+                "verpacken"
+            ]
+        },
+        {
+            "English": [
+                "shipping"
+            ],
+            "Deutsch": [
+                "der Versand"
+            ]
+        },
+        {
+            "English": [
+                "stuff"
+            ],
+            "Deutsch": [
+                "das Zeug, der Kram"
+            ]
+        },
+        {
+            "English": [
+                "track"
+            ],
+            "Deutsch": [
+                "nachverfolgen"
+            ]
+        },
+        {
+            "English": [
+                "afford"
+            ],
+            "Deutsch": [
+                "sich leisten"
+            ]
+        },
+        {
+            "English": [
+                "budget"
+            ],
+            "Deutsch": [
+                "das Budget, der Etat"
+            ]
+        },
+        {
+            "English": [
+                "change"
+            ],
+            "Deutsch": [
+                "das Wechselgeld"
+            ]
+        },
+        {
+            "English": [
+                "checkout"
+            ],
+            "Deutsch": [
+                "die Kasse"
+            ]
+        },
+        {
+            "English": [
+                "deal"
+            ],
+            "Deutsch": [
+                "das Gesch\u00e4ft, das Angebot"
+            ]
+        },
+        {
+            "English": [
+                "goods"
+            ],
+            "Deutsch": [
+                "die Waren"
+            ]
+        },
+        {
+            "English": [
+                "purchase"
+            ],
+            "Deutsch": [
+                "der Kauf"
+            ]
+        },
+        {
+            "English": [
+                "receipt"
+            ],
+            "Deutsch": [
+                "der Beleg, die Quittung"
+            ]
+        },
+        {
+            "English": [
+                "special offer"
+            ],
+            "Deutsch": [
+                "das Sonderangebot"
+            ]
+        },
+        {
+            "English": [
+                "cart"
+            ],
+            "Deutsch": [
+                "das Fuhrwerk"
+            ]
+        },
+        {
+            "English": [
+                "chest"
+            ],
+            "Deutsch": [
+                "die Truhe"
+            ]
+        },
+        {
+            "English": [
+                "copper"
+            ],
+            "Deutsch": [
+                "das Kupfer"
+            ]
+        },
+        {
+            "English": [
+                "date"
+            ],
+            "Deutsch": [
+                "die Dattel"
+            ]
+        },
+        {
+            "English": [
+                "fig"
+            ],
+            "Deutsch": [
+                "die Feige"
+            ]
+        },
+        {
+            "English": [
+                "merchant"
+            ],
+            "Deutsch": [
+                "der/die H\u00e4ndler/-in"
+            ]
+        },
+        {
+            "English": [
+                "olive"
+            ],
+            "Deutsch": [
+                "die Olive"
+            ]
+        },
+        {
+            "English": [
+                "slogan"
+            ],
+            "Deutsch": [
+                "der Werbeslogan, der Wahlspruch"
+            ]
+        },
+        {
+            "English": [
+                "sword"
+            ],
+            "Deutsch": [
+                "das Schwert"
+            ]
+        },
+        {
+            "English": [
+                "available"
+            ],
+            "Deutsch": [
+                "verf\u00fcgbar"
+            ]
+        },
+        {
+            "English": [
+                "courteous"
+            ],
+            "Deutsch": [
+                "h\u00f6flich, zuvorkommend"
+            ]
+        },
+        {
+            "English": [
+                "delivery"
+            ],
+            "Deutsch": [
+                "die Auslieferung"
+            ]
+        },
+        {
+            "English": [
+                "exchange"
+            ],
+            "Deutsch": [
+                "tauschen"
+            ]
+        },
+        {
+            "English": [
+                "gift-wrap"
+            ],
+            "Deutsch": [
+                "als Geschenk verpacken"
+            ]
+        },
+        {
+            "English": [
+                "model"
+            ],
+            "Deutsch": [
+                "die Ausf\u00fchrung, die Variante"
+            ]
+        },
+        {
+            "English": [
+                "reference number"
+            ],
+            "Deutsch": [
+                "die Referenznummer"
+            ]
+        },
+        {
+            "English": [
+                "return"
+            ],
+            "Deutsch": [
+                "die R\u00fcckgabe"
+            ]
+        },
+        {
+            "English": [
+                "stock"
+            ],
+            "Deutsch": [
+                "die Lagerware"
+            ]
+        },
+        {
+            "English": [
+                "tag"
+            ],
+            "Deutsch": [
+                "das Etikett"
+            ]
+        },
+        {
+            "English": [
+                "till"
+            ],
+            "Deutsch": [
+                "die Ladenkasse"
+            ]
+        },
+        {
+            "English": [
+                "arrange"
+            ],
+            "Deutsch": [
+                "abmachen"
+            ]
+        },
+        {
+            "English": [
+                "cheat"
+            ],
+            "Deutsch": [
+                "betr\u00fcgen"
+            ]
+        },
+        {
+            "English": [
+                "fixed"
+            ],
+            "Deutsch": [
+                "fest, festgelegt"
+            ]
+        },
+        {
+            "English": [
+                "kaftan"
+            ],
+            "Deutsch": [
+                "der Kaftan"
+            ]
+        },
+        {
+            "English": [
+                "pressure"
+            ],
+            "Deutsch": [
+                "bedr\u00e4ngen, unter Druck setzen"
+            ]
+        },
+        {
+            "English": [
+                "vendor"
+            ],
+            "Deutsch": [
+                "der/die Verk\u00e4ufer/-in, der/die Anbieter/-in"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "English_L9",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
