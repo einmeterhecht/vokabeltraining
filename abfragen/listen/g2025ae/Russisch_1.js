3983d0103539e934ba80e6b4952c01903e38604a
einmeterhecht <119974780+einmeterhecht@users.noreply.github.com>
2026-09-22 08:24:52 +0100

    add

---
+++ abfragen/listen/g2025ae/Russisch_1.js
@@ -0,0 +1,364 @@
+var liste = {
+    "fragen": [
+        {
+            "Russisch": [
+                "\u043b\u0435\u0442\u043d\u0438\u0439, \u043b\u0435\u0442\u043d\u044f\u044f \u043b\u0435\u0442\u043d\u0435\u0435, \u043b\u0435\u0442\u043d\u0438\u0435"
+            ],
+            "Deutsch": [
+                "Sommer-"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043a\u0430\u043d\u0438\u043a\u0443\u043b\u044b"
+            ],
+            "Deutsch": [
+                "(Schul)ferien"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0434\u0435\u0442\u0441\u043a\u0438\u0439, \u0434\u0435\u0442\u0441\u043a\u0430\u044f, \u0434\u0435\u0442\u0441\u043a\u043e\u0435, \u0434\u0435\u0442\u0441\u043a\u0438\u0435"
+            ],
+            "Deutsch": [
+                "Kinder-"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043b\u0430\u0433\u0435\u0440\u044c"
+            ],
+            "Deutsch": [
+                "Lager"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0432\u0435\u0441\u0451\u043b\u044b\u0439, \u0432\u0435\u0441\u0451\u043b\u0430\u044f,\n\u0432\u0435\u0441\u0451\u043b\u043e\u0435, \u0432\u0435\u0441\u0451\u043b\u044b\u0435"
+            ],
+            "Deutsch": [
+                "lustig, heiter (Adj.)"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0432\u0435\u0441\u0435\u043b\u043e"
+            ],
+            "Deutsch": [
+                "lustig, heiter (Adv.)"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0438\u0433\u0440\u0430"
+            ],
+            "Deutsch": [
+                "Spiel"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0437\u0430\u0433\u043e\u0440\u0430\u0442\u044c"
+            ],
+            "Deutsch": [
+                "sich sonnen, in der Sonne liegen"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0431\u0435\u0440\u0435\u0433 (\u041f\u0440\u0435\u043f.: \u043d\u0430 \u0431\u0435\u0440\u0435\u0433\u0443)"
+            ],
+            "Deutsch": [
+                "Ufer"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043f\u043b\u0430\u0432\u0430\u0442\u044c"
+            ],
+            "Deutsch": [
+                "schwimmen"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043b\u043e\u0432\u0438\u0442\u044c \u0440\u044b\u0431\u0443"
+            ],
+            "Deutsch": [
+                "fischen"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0441\u043e\u0431\u0438\u0440\u0430\u0442\u044c"
+            ],
+            "Deutsch": [
+                "sammeln; (packen)"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0433\u0440\u0438\u0431"
+            ],
+            "Deutsch": [
+                "Pilz"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u044f\u0433\u043e\u0434\u0430"
+            ],
+            "Deutsch": [
+                "Beere"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043b\u0435\u0441 (\u0432 \u043b\u0435\u0441\u0443)"
+            ],
+            "Deutsch": [
+                "Wald(im Wald)"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0445\u043e\u0434\u0438\u0442\u044c \u0432 \u043f\u043e\u0445\u043e\u0434\u044b"
+            ],
+            "Deutsch": [
+                "wandern gehen"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0442\u0430\u043d\u0446\u0435\u0432\u0430\u0442\u044c (\u0442\u0430\u043d\u0446\u0443\u044e, \u0442\u0430\u043d\u0446\u0443\u0435\u0448\u044c, \u0442\u0430\u043d\u0446\u0443\u044e\u0442)"
+            ],
+            "Deutsch": [
+                "tanzen"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0434\u0438\u0441\u043a\u043e\u0442\u0435\u043a\u0430"
+            ],
+            "Deutsch": [
+                "Disko"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0410 \u043c\u043e\u0436\u0435\u0442 \u043d\u0435\u0442?"
+            ],
+            "Deutsch": [
+                "Vielleicht nicht"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043f\u0440\u043e\u0432\u043e\u0434\u0438\u0442\u044c \u0432\u0440\u0435\u043c\u044f"
+            ],
+            "Deutsch": [
+                "Zeit verbringen"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043f\u0438\u043d\u0433-\u043f\u043e\u043d\u0433"
+            ],
+            "Deutsch": [
+                "Ping-Pong"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043c\u044f\u0447"
+            ],
+            "Deutsch": [
+                "Ball"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043c\u0430\u043b\u0438\u043d\u0430"
+            ],
+            "Deutsch": [
+                "Himbeere"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043a\u043b\u0443\u0431\u043d\u0438\u043a\u0430"
+            ],
+            "Deutsch": [
+                "Erdbeere"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0435\u0436\u0435\u0432\u0438\u043a\u0430"
+            ],
+            "Deutsch": [
+                "Brombeere"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0433\u043e\u043b\u0443\u0431\u0438\u043a\u0430"
+            ],
+            "Deutsch": [
+                "Blaubeere"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0432\u0438\u0448\u043d\u044f"
+            ],
+            "Deutsch": [
+                "Sauerkirsche"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0447\u0435\u0440\u0435\u0448\u043d\u044f"
+            ],
+            "Deutsch": [
+                "S\u00fc\u00dfkirsche"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u044f\u0431\u043b\u043e\u043a\u043e (\u044f\u0431\u043b\u043e\u043a\u0438)"
+            ],
+            "Deutsch": [
+                "Apfel, \u00c4pfel"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0433\u0440\u0443\u0448\u0430"
+            ],
+            "Deutsch": [
+                "Birne"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0430\u0431\u0440\u0438\u043a\u043e\u0441"
+            ],
+            "Deutsch": [
+                "Aprikose"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043f\u0435\u0440\u0441\u0438\u043a"
+            ],
+            "Deutsch": [
+                "Pfirsich"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0441\u043b\u0438\u0432\u0430"
+            ],
+            "Deutsch": [
+                "Pflaume, Zwetschge"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0430\u0440\u0431\u0443\u0437"
+            ],
+            "Deutsch": [
+                "Wassermelone"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0434\u044b\u043d\u044f"
+            ],
+            "Deutsch": [
+                "Melone"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043f\u0440\u0438\u0440\u043e\u0434\u0430, \u043d\u0430 \u043f\u0440\u0438\u0440\u043e\u0434\u0435"
+            ],
+            "Deutsch": [
+                "Natur"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0437\u0430 \u0433\u0440\u0430\u043d\u0438\u0446\u0435\u0439"
+            ],
+            "Deutsch": [
+                "im Ausland"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0433\u0440\u0430\u043d\u0438\u0446\u0430"
+            ],
+            "Deutsch": [
+                "Grenze"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0434\u043e\u043c\u0430"
+            ],
+            "Deutsch": [
+                "zuhause, zu Hause"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0432 \u0433\u043e\u0440\u0430\u0445"
+            ],
+            "Deutsch": [
+                "in den Bergen, im Gebirge"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u0441\u0442\u0440\u0430\u043d\u0430, \u0441\u0442\u0440\u0430\u043d\u044b"
+            ],
+            "Deutsch": [
+                "Land"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043f\u043e\u0435\u0445\u0430\u0442\u044c"
+            ],
+            "Deutsch": [
+                "fahren (einmal)"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043a\u0443\u0434\u0430"
+            ],
+            "Deutsch": [
+                "wohin"
+            ]
+        },
+        {
+            "Russisch": [
+                "\u043e\u0442\u043a\u0443\u0434\u0430"
+            ],
+            "Deutsch": [
+                "woher"
+            ]
+        }
+    ],
+    "hinweis_attribute": [
+        "Beispiele",
+        "Lernhilfe"
+    ],
+    "titel": "Russisch_1",
+    "frage_attribut": "Deutsch"
+};
+
+erste_aufgabe();
